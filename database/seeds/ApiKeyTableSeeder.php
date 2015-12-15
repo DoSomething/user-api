95e783a8cd8d6e6a604aa19f4948a5c63a2cbf9c
David Furnes <david@dfurnes.com>
2015-12-15 14:35:24 -0500

    Remove admin scope from second API key.

--- database/seeds/ApiKeyTableSeeder.php
+++ database/seeds/ApiKeyTableSeeder.php
@@ -14,16 +14,18 @@ class ApiKeyTableSeeder extends Seeder
     {
         DB::table('api_keys')->delete();
 
+        // An API key with admin (privileged) scope.
         ApiKey::create([
             'app_id' => '456',
             'api_key' => 'abc4324',
             'scope' => ['admin', 'user'],
         ]);
 
+        // An API key with default user (non-privileged) scope.
         ApiKey::create([
             'app_id' => '123',
             'api_key' => '5464utyrs',
-            'scope' => 'admin'
+            'scope' => ['user']
         ]);
     }
 }
