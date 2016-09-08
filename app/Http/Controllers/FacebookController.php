5decd3f3b902982d97704994b7bd284b85535124
deadlybutter <thedeadlybutter@gmail.com>
2016-09-08 15:37:20 -0400

    Style fixes

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -4,7 +4,6 @@ namespace Northstar\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
 
 class FacebookController extends Controller
 {
@@ -18,26 +17,27 @@ class FacebookController extends Controller
         $this->client_id = config('services.facebook.client_id');
 
         $this->client = new Client([
-            'base_uri' => $base_url
+            'base_uri' => $base_url,
         ]);
     }
 
-    public function validateToken(Request $request) {
+    public function validateToken(Request $request)
+    {
         $this->validate($request, [
             'input_token' => 'required',
             'facebook_id' => 'required',
         ]);
 
         $response = $this->client->request('GET', 'debug_token', [
-            'query' => ['access_token' => $this->client_id . '|' . $this->client_secret, 'input_token' => $request->input('input_token')],
+            'query' => ['access_token' => $this->client_id.'|'.$this->client_secret, 'input_token' => $request->input('input_token')],
         ]);
 
         $verification = json_decode($response->getBody()->getContents())->data;
 
         if ($verification->is_valid && $verification->user_id == $request->input('facebook_id')) {
-            return $this->respond("Verified", 200);
+            return $this->respond('Verified', 200);
         }
 
-        return $this->respond("Invalid", 401);
+        return $this->respond('Invalid', 401);
     }
 }
