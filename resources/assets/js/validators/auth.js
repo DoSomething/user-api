274fcb6de509cdbffbe846e5969149d58bfa3bac
deadlybutter <thedeadlybutter@gmail.com>
2017-01-30 13:00:41 -0500

    Removes drupal function from auth validation

--- resources/assets/js/validators/auth.js
+++ resources/assets/js/validators/auth.js
@@ -1,15 +1,15 @@
 define(function(require) {
-  "use strict";
+  'use strict';
 
   const Validation = require('dosomething-validation');
 
   // We use Mailcheck (https://github.com/mailcheck/mailcheck) to provide
-  // suggestions for typos in email addresses. See the "email" validator below.
-  var Mailcheck = require("mailcheck");
+  // suggestions for typos in email addresses. See the 'email' validator below.
+  var Mailcheck = require('mailcheck');
 
   // We'll add a few extra domains to the defaults provided.
   // @see: https://github.com/mailcheck/mailcheck/blob/v1.1.1/src/mailcheck.js#L16-L25
-  Mailcheck.defaultDomains.push("dosomething.org", "sina.com");
+  Mailcheck.defaultDomains.push('dosomething.org', 'sina.com');
 
   // # Helpers
   // Basic sanity check used by email validation.
@@ -17,10 +17,10 @@ define(function(require) {
   function isValidEmailSyntax(string) {
     var email = string.toUpperCase();
     if( email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/) ) {
-      var lastChar = "";
+      var lastChar = '';
       for(var i = 0, len = email.length; i < len; i++) {
         // fail if we see two dots in a row
-        if(lastChar === "."  && email[i] === ".") {
+        if(lastChar === '.'  && email[i] === '.') {
           return false;
         }
 
@@ -59,29 +59,29 @@ define(function(require) {
 
   // ## Birthday
   // Validates correct date input, reasonable birthdate, and says a nice message.
-  Validation.registerValidationFunction("birthday", function(string, done) {
+  Validation.registerValidationFunction('birthday', function(string, done) {
     var birthday, birthMonth, birthDay, birthYear, format;
 
     // Date validation format
     format = 'MM/DD/YYYY';
 
     // parse date from string
-    if( format === "MM/DD/YYYY" && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
+    if( format === 'MM/DD/YYYY' && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
       // US formatting
-      birthday = string.split("/");
+      birthday = string.split('/');
       birthMonth = parseInt(birthday[0]);
       birthDay = parseInt(birthday[1]);
       birthYear = parseInt(birthday[2]);
-    } else if( format === "DD/MM/YYYY" && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
+    } else if( format === 'DD/MM/YYYY' && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
       // UK formatting
-      birthday = string.split("/");
+      birthday = string.split('/');
       birthDay = parseInt(birthday[0]);
       birthMonth = parseInt(birthday[1]);
       birthYear = parseInt(birthday[2]);
     } else {
       return done({
         success: false,
-        message: Drupal.t("Enter your birthday " + format + "!")
+        message: `Enter your birthday ${format} !`,
       });
     }
 
@@ -89,7 +89,7 @@ define(function(require) {
     if (birthMonth > 12 || birthMonth === 0) {
       return done({
         success: false,
-        message: Drupal.t("That doesn't seem right.")
+        message: 'That doesn\'t seem right.',
       });
     }
 
@@ -103,7 +103,7 @@ define(function(require) {
     if (birthDay > endDates[birthMonth]) {
       return done({
         success: false,
-        message: Drupal.t("That doesn't seem right.")
+        message: 'That doesn\'t seem right.',
       });
     }
 
@@ -120,41 +120,41 @@ define(function(require) {
     if (age < 0)  {
       return done({
         success: false,
-        message: Drupal.t("Are you a time traveller?")
+        message: 'Are you a time traveller?',
       });
     } else if( age > 0 && age <= 25  ) {
 
       if (birthDate.getMonth() === now.getMonth() && now.getDate() === birthDate.getDate() ) {
         return done({
           success: true,
-          message: Drupal.t("Wow, happy birthday!")
+          message: 'Wow, happy birthday!',
         });
       } else if ( age < 10) {
         return done({
           success: true,
-          message: Drupal.t("Wow, you're @age!", {"@age": age})
+          message: `Wow, you're ${age}!`,
         });
       } else {
         return done({
           success: true,
-          message: Drupal.t("Cool, @age!", {"@age": age})
+          message: `Cool, ${age}!`,
         });
       }
 
     } else if (age > 25 && age < 130) {
       return done({
         success: true,
-        message: Drupal.t("Got it!")
+        message: 'Got it!',
       });
-    } else if (string === "") {
+    } else if (string === '') {
       return done({
         success: false,
-        message: Drupal.t("We need your birthday.")
+        message: 'We need your birthday.',
       });
     } else {
       return done({
         success: false,
-        message: Drupal.t("That doesn't seem right.")
+        message: 'That doesn\'t seem right.',
       });
     }
   });
@@ -162,7 +162,7 @@ define(function(require) {
   // ## Email
   // Performs some basic sanity checks on email format (see helper method), and
   // uses Kicksend's mailcheck library to offer suggestions for misspellings.
-  Validation.registerValidationFunction("email", function(string, done) {
+  Validation.registerValidationFunction('email', function(string, done) {
     if ( isValidEmailSyntax(string) ) {
       // We use mailcheck.js to find some common email mispellings
       Mailcheck.run({
@@ -176,14 +176,14 @@ define(function(require) {
         empty: function() {
           return done({
             success: true,
-            message: Drupal.t("Great, thanks!")
+            message: 'Great, thanks!',
           });
         }
       });
     } else {
       return done({
         success: false,
-        message: Drupal.t("We need a valid email.")
+        message: 'We need a valid email.',
       });
     }
   });
@@ -191,39 +191,39 @@ define(function(require) {
   // ## Password
   // Checks that password is 6 or more characters long.
   // Matches validation performed in dosomething_user.module.
-  Validation.registerValidationFunction("password", function(string, done) {
+  Validation.registerValidationFunction('password', function(string, done) {
     if(string.length >= 6) {
       return done({
         success: true,
-        message: Drupal.t("Keep it secret, keep it safe!")
+        message: 'Keep it secret, keep it safe!',
       });
     } else {
       return done({
         success: false,
-        message: Drupal.t("Must be 6+ characters.")
+        message: 'Must be 6+ characters.',
       });
     }
   });
 
   // ## Phone
   // Matches validation performed in dosomething_user.module.
-  Validation.registerValidationFunction("phone", function(string, done) {
+  Validation.registerValidationFunction('phone', function(string, done) {
     // Matches server-side validation from `dosomething_user_valid_cell()` in `dosomething_user.module`.
-    var numberWithoutFormatting = string.replace(/[\-\s\.]/g, "");
+    var numberWithoutFormatting = string.replace(/[\-\s\.]/g, '');
     var isValidFormat = /^(?:\+?([0-9]{1,3})([\-\s\.]{1})?)?\(?([0-9]{3})\)?(?:[\-\s\.]{1})?([0-9]{3})(?:[\-\s\.]{1})?([0-9]{4})/.test(numberWithoutFormatting);
 
-    var sanitizedNumber = string.replace(/[^0-9]/g, "");
+    var sanitizedNumber = string.replace(/[^0-9]/g, '');
     var allRepeatingDigits = /([0-9]{1})\1{9,}/.test(sanitizedNumber);
 
     if(isValidFormat && !allRepeatingDigits) {
       return done({
         success: true,
-        message: Drupal.t("Thanks!")
+        message: 'Thanks!',
       });
     } else {
       return done({
         success: false,
-        message: Drupal.t("Enter a valid telephone number.")
+        message: 'Enter a valid telephone number.',
       });
     }
   });
