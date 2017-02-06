0087ddda50cc585126ef9b1a8067b518facae3d3
deadlybutter <thedeadlybutter@gmail.com>
2017-02-06 14:20:52 -0500

    Renaming test case, adding a unsupported country code test

--- tests/Http/LocalizationTest.php
+++ tests/Http/LocalizationTest.php
@@ -6,9 +6,14 @@ class LocalizationTest extends TestCase
     /**
      * Test that the correct Fastly header is applied for the given header.
      */
-    public function testAppLocale()
+    public function testSupportedCountry()
     {
         $this->get('/', ['X-FASTLY-COUNTRY-CODE' => 'MX']);
         $this->assertEquals(App::getLocale(), 'es-mx');
+    }
+
+    public function testUnsupportedCountry() {
+        $this->get('/', ['X-FASTLY-COUNTRY-CODE' => 'FR']);
+        $this->assertEquals(App::getLocale(), 'en');
     }
 }
