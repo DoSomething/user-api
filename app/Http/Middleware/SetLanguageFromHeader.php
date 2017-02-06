355ddf14ab7bef55fd439623a0169b58de22047f
deadlybutter <thedeadlybutter@gmail.com>
2017-02-06 15:57:48 -0500

    Adds Javascript to page containing the language

--- app/Http/Middleware/SetLanguageFromHeader.php
+++ app/Http/Middleware/SetLanguageFromHeader.php
@@ -18,19 +18,25 @@ class SetLanguageFromHeader
     public function handle($request, Closure $next)
     {
         $countryCode = strtolower($request->header('X-Fastly-Country-Code'));
+
         if ($countryCode) {
             switch ($countryCode) {
                 case 'us':
-                    App::setLocale('en');
+                    $language = 'en';
                     break;
                 case 'mx':
-                    App::setLocale('es-mx');
+                    $language = 'es-mx';
                     break;
                 default:
-                    App::setLocale('en');
+                    $language = 'en';
                     break;
             }
         }
+
+        App::setLocale($language);
+        app('JavaScript')->put([
+            'language' => $language,
+        ]);
 
         return $next($request);
     }
