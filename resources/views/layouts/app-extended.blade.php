e680092a8fd6d81d52d2f3c8c89bbc47377714e2
deadlybutter <thedeadlybutter@gmail.com>
2017-07-25 17:15:07 -0400

    Adding extended layout to register page

---
+++ resources/views/layouts/app-extended.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>@yield('title', 'DoSomething.org')</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('apple-touch-icon-precomposed.png') }}">
+
+    <link rel="stylesheet" href="{{ elixir('app.css', 'dist') }}">
+    <script src="{{ asset('dist/modernizr.js') }}"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body class="modernizr-no-js">
+    <div class="chrome">
+        @if (session('status'))
+            <div class="messages">{{ session('status') }}</div>
+        @endif
+        <div class="wrapper">
+            @include('layouts.navigation', ['extended' => true])
+
+            <section class="container -framed -extended">
+
+                <div class="wrapper">
+
+                @yield('content')
+
+                </div>
+            </section>
+
+        </div>
+    </div>
+
+    @include('layouts.variables')
+    <script src="{{ elixir('app.js', 'dist') }}"></script>
+    @include('layouts.google_analytics')
+</body>
+
+</html>
