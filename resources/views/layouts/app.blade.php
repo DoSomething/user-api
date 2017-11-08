00bc013a8cbcc2b90cbee84486b0d166e0f34f0e
deadlybutter <thedeadlybutter@gmail.com>
2017-11-08 14:56:52 -0500

    adding errors to the page js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
     @include('layouts.variables')
     {{ scriptify(auth()->user() ? auth()->user()->id : null, 'NORTHSTAR_ID') }}
     {{ scriptify(get_client_environment_vars(), 'ENV') }}
+    {{ scriptify(count($errors) > 0 ? $errors->default : null, 'ERRORS') }}
     <script src="{{ elixir('app.js', 'dist') }}"></script>
     @include('layouts.google_analytics')
 </body>
