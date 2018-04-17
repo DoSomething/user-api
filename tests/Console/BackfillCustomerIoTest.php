06dcdba3a658d203b0a31cf84743bd3ff7f61671
katiecrane <kcrane@dosomething.org>
2018-04-17 14:07:32 -0400

    Update to backfill test

--- tests/Console/BackfillCustomerIoTest.php
+++ tests/Console/BackfillCustomerIoTest.php
@@ -9,22 +9,12 @@ class BackfillCustomerIoTest extends BrowserKitTestCase
     /** @test */
     public function it_should_backfill_users()
     {
-        // Bus::fake();
-
         $users = factory(User::class, 5)->create();
-
-        // with this there is really no way to tell that each got sent over twice
-        // Mock SendUserToCustomerIo job & make sure it gets called for each new user
-        // foreach ($users as $user) {
-        //     Bus::assertDispatched(SendUserToCustomerIo::class, function ($job) use ($user) {
-        //             return $user->id === $job->getUserId();
-        //     });
-        // }
-
-        // I think that this is the best way to test this (5 times when created, 5 times when we call the command)
-        $this->blinkMock->shouldHaveReceived('userCreate')->times(10);
 
         // Run the Customer.io backfill command.
         $this->artisan('northstar:cio');
+
+        // I think that this is the best way to test this (5 times when created, 5 times when we call the command)
+        $this->blinkMock->shouldHaveReceived('userCreate')->times(10);
     }
 }
