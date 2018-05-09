f2f66284ebc2a813371f388a11d1cbce0e1b2b2e
katiecrane <kcrane@dosomething.org>
2018-05-09 14:39:07 -0400

    Migration to rename email_status to email_frequency

---
+++ database/migrations/2018_05_09_183441_RenameEmailStatusField.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class RenameEmailStatusField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->renameField('users', 'email_status', 'email_frequency');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->renameField('users', 'email_frequency', 'email_status');
+    }
+
+    /**
+     * Rename the given field on any documents in the collection.
+     *
+     * @param string $collection
+     * @param string $old
+     * @param string $new
+     */
+    public function renameField($collection, $old, $new)
+    {
+        /** @var \Jenssegers\Mongodb\Connection $connection */
+        $connection = app('db')->connection('mongodb');
+
+        // Rename 'mobile_status' to 'mobilecommons_status'.
+        $connection->collection($collection)
+            ->whereRaw([$old => ['$exists' => true]])
+            ->update(['$rename' => [$old => $new]]);
+    }
+}
