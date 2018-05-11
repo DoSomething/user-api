dff5c615abbc4d8376e89c5d23a94402bf1250aa
katiecrane <kcrane@dosomething.org>
2018-05-11 16:23:23 -0400

    Add test for command

---
+++ tests/Console/UnsetFieldsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use Northstar\Models\User;
+
+class UnsetFieldsTest extends TestCase
+{
+     /** @test */
+    public function it_should_remove_fields()
+    {
+        // Create some users
+        factory(User::class, 5)->create();
+
+        // Make sure all users have a `city`
+        $usersWithCity = User::whereRaw([
+            'city' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithCity, 5);
+
+        // Make sure all users have a `sms_status`
+        $usersWithSmsStatus = User::whereRaw([
+            'sms_status' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithSmsStatus, 5);
+
+        // Make sure all users have a `country`
+        $usersWithCountry = User::whereRaw([
+            'country' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithCountry, 5);
+
+        // Run the command to unset `city` and `country`
+        $this->artisan('northstar:unset', ['field' => ['city', 'country'], '--force' => true]);
+
+        // Make sure NO users have a `city`
+        $usersWithCity = User::whereRaw([
+            'city' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithCity, 0);
+
+        // Make sure all users have a `sms_status`
+        $usersWithSmsStatus = User::whereRaw([
+            'sms_status' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithSmsStatus, 5);
+
+        // Make sure NO users have a `country`
+        $usersWithCountry = User::whereRaw([
+            'country' => [
+                '$exists' => true,
+            ],
+        ])->count();
+        $this->assertEquals($usersWithCountry, 0);
+    }
+}
