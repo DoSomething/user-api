60bc06c0e231bf493ce7a5232f8532a3278a60d7
katiecrane <kcrane@dosomething.org>
2018-05-11 11:54:30 -0400

    Use chunking in command

--- app/Console/Commands/UnsetFieldCommand.php
+++ app/Console/Commands/UnsetFieldCommand.php
@@ -2,6 +2,7 @@
 
 namespace Northstar\Console\Commands;
 
+use Northstar\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
 
@@ -39,18 +40,27 @@ class UnsetFieldCommand extends Command
     public function handle()
     {
         $fieldsToRemove = $this->argument('field');
-        /** @var \Jenssegers\Mongodb\Connection $connection */
-        $connection = app('db')->connection('mongodb');
 
         foreach ($fieldsToRemove as $field) {
             $burnItDown = $this->confirm('Are you sure you want to remove this field from ALL USERS? `'.$field.'`');
 
             if ($burnItDown) {
                 info('Removing field from all users: '.$field);
-                // Unset $field
-                $connection->collection('users')
-                    ->whereRaw([$field => ['$exists' => true]])
-                    ->unset([$field]);
+
+                $usersToUnset = (new User)->newQuery();
+                $usersToUnset->whereRaw([$field => ['$exists' => true]]);
+
+                $this->line('Progess removing '.$field.':');
+                $progressBar = $this->output->createProgressBar($usersToUnset->count());
+
+                $usersToUnset->chunkById(200, function (Collection $users) use ($progressBar, $field) {
+                    $users->each(function (User $user) use ($progressBar, $field) {
+                        $user->unset([$field]);
+                        $progressBar->advance();
+                    });
+                });
+
+                $progressBar->finish();
                 info('Field removed: '.$field);
             }
         }
