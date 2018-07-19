0af5dfa79e6c3a36c53a7ae3fc9ab2bc98d47fa9
David Furnes <david@dfurnes.com>
2018-07-19 11:09:13 -0400

    Add middleware to log memory usage.

---
+++ app/Http/Middleware/LogMemoryUsage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Northstar\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class LogMemoryUsage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param array $roles
+     * @return mixed
+     * @throws OAuthServerException
+     * @internal param $role
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        $response = $next($request);
+
+        // Log how much memory this request used.
+        $megabytes = memory_get_peak_usage() / 1000000;
+        Log::debug('memory_usage', ['mb' => $megabytes, 'path' => $request->path()]);
+
+        return $response;
+    }
+}
