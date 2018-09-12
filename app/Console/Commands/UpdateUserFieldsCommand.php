116d16e2e3dedf8875d7e971f52b388c9276bd4e
katiecrane <kcrane@dosomething.org>
2018-09-12 12:23:54 -0700

    Fix error introduced by following style rules

--- app/Console/Commands/UpdateUserFieldsCommand.php
+++ app/Console/Commands/UpdateUserFieldsCommand.php
@@ -94,7 +94,8 @@ class UpdateUserFieldsCommand extends Command
 
             if ($this->option('verbose')) {
                 info('northstar:update: Updated user - '.$user->id);
-                info('northstar:update: '.memory_get_peak_usage() / 1000000.' Mb used');
+                $mb = memory_get_peak_usage() / 1000000;
+                info('northstar:update: '.$mb.' Mb used');
             }
 
             $this->logPercent();
