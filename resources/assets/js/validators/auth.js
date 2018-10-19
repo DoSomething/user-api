1941c980185e4b9773566f1e2be7e925b0adc25f
katiecrane <kcrane@dosomething.org>
2018-10-19 15:57:31 -0700

    Only made a few changes in this file why is everything changed??

--- resources/assets/js/validators/auth.js
+++ resources/assets/js/validators/auth.js
@@ -1,23 +1,23 @@
-const Validation = require('dosomething-validation');
+const Validation = require("dosomething-validation");
 
 // We use Mailcheck (https://github.com/mailcheck/mailcheck) to provide
 // suggestions for typos in email addresses. See the 'email' validator below.
-var Mailcheck = require('mailcheck');
+var Mailcheck = require("mailcheck");
 
 // We'll add a few extra domains to the defaults provided.
 // @see: https://github.com/mailcheck/mailcheck/blob/v1.1.1/src/mailcheck.js#L16-L25
-Mailcheck.defaultDomains.push('dosomething.org', 'sina.com');
+Mailcheck.defaultDomains.push("dosomething.org", "sina.com");
 
 // # Helpers
 // Basic sanity check used by email validation.
 // This won't catch everything, but should prevent validating some simple mistakes
 function isValidEmailSyntax(string) {
   var email = string.toUpperCase();
-  if( email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/) ) {
-    var lastChar = '';
-    for(var i = 0, len = email.length; i < len; i++) {
+  if (email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/)) {
+    var lastChar = "";
+    for (var i = 0, len = email.length; i < len; i++) {
       // fail if we see two dots in a row
-      if(lastChar === '.'  && email[i] === '.') {
+      if (lastChar === "." && email[i] === ".") {
         return false;
       }
 
@@ -32,7 +32,7 @@ function isValidEmailSyntax(string) {
 
 // @TODO: This will be removed when Neue's validation gets refactored.
 function validateNotBlank(string, done, success, failure) {
-  if( string !== '' ) {
+  if (string !== "") {
     return done({
       success: true,
       message: success
@@ -47,38 +47,38 @@ function validateNotBlank(string, done, success, failure) {
 
 // ## First Name
 // Greets the user when they enter their first name.
-Validation.registerValidationFunction('first_name', function(string, done) {
-  validateNotBlank(string, done,
-    `Hey, ${string}`,
-    'We need your first name.'
-  );
+Validation.registerValidationFunction("first_name", function(string, done) {
+  validateNotBlank(string, done, `Hey, ${string}`, "We need your first name.");
 });
 
 // ## Birthday
 // Validates correct date input, reasonable birthdate, and says a nice message.
-Validation.registerValidationFunction('birthday', function(string, done) {
+Validation.registerValidationFunction("birthday", function(string, done) {
   var birthday, birthMonth, birthDay, birthYear, format;
 
   // Date validation format
-  format = 'MM/DD/YYYY';
+  format = "MM/DD/YYYY";
 
   // parse date from string
-  if( format === 'MM/DD/YYYY' && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
+  if (format === "MM/DD/YYYY" && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string)) {
     // US formatting
-    birthday = string.split('/');
+    birthday = string.split("/");
     birthMonth = parseInt(birthday[0]);
     birthDay = parseInt(birthday[1]);
     birthYear = parseInt(birthday[2]);
-  } else if( format === 'DD/MM/YYYY' && /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string) ) {
+  } else if (
+    format === "DD/MM/YYYY" &&
+    /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(string)
+  ) {
     // UK formatting
-    birthday = string.split('/');
+    birthday = string.split("/");
     birthDay = parseInt(birthday[0]);
     birthMonth = parseInt(birthday[1]);
     birthYear = parseInt(birthday[2]);
   } else {
     return done({
       success: false,
-      message: `Enter your birthday ${format} !`,
+      message: `Enter your birthday ${format} !`
     });
   }
 
@@ -86,13 +86,13 @@ Validation.registerValidationFunction('birthday', function(string, done) {
   if (birthMonth > 12 || birthMonth === 0) {
     return done({
       success: false,
-      message: 'That doesn\'t seem right.',
+      message: "That doesn't seem right."
     });
   }
 
   //list of last days in months and check for leap year
   var endDates = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if(((birthYear % 4 === 0) && (birthYear % 100 !== 0)) || (birthYear % 400 === 0)){
+  if ((birthYear % 4 === 0 && birthYear % 100 !== 0) || birthYear % 400 === 0) {
     endDates[2] = 29;
   }
 
@@ -100,7 +100,7 @@ Validation.registerValidationFunction('birthday', function(string, done) {
   if (birthDay > endDates[birthMonth]) {
     return done({
       success: false,
-      message: 'That doesn\'t seem right.',
+      message: "That doesn't seem right."
     });
   }
 
@@ -114,44 +114,45 @@ Validation.registerValidationFunction('birthday', function(string, done) {
     age--;
   }
 
-  if (age < 0)  {
+  if (age < 0) {
     return done({
       success: false,
-      message: 'Are you a time traveller?',
+      message: "Are you a time traveller?"
     });
-  } else if( age > 0 && age <= 25  ) {
-
-    if (birthDate.getMonth() === now.getMonth() && now.getDate() === birthDate.getDate() ) {
+  } else if (age > 0 && age <= 25) {
+    if (
+      birthDate.getMonth() === now.getMonth() &&
+      now.getDate() === birthDate.getDate()
+    ) {
       return done({
         success: true,
-        message: 'Wow, happy birthday!',
+        message: "Wow, happy birthday!"
       });
-    } else if ( age < 10) {
+    } else if (age < 10) {
       return done({
         success: true,
-        message: `Wow, you're ${age}!`,
+        message: `Wow, you're ${age}!`
       });
     } else {
       return done({
         success: true,
-        message: `Cool, ${age}!`,
+        message: `Cool, ${age}!`
       });
     }
-
   } else if (age > 25 && age < 130) {
     return done({
       success: true,
-      message: 'Got it!',
+      message: "Got it!"
     });
-  } else if (string === '') {
+  } else if (string === "") {
     return done({
       success: false,
-      message: 'We need your birthday.',
+      message: "We need your birthday."
     });
   } else {
     return done({
       success: false,
-      message: 'That doesn\'t seem right.',
+      message: "That doesn't seem right."
     });
   }
 });
@@ -159,8 +160,8 @@ Validation.registerValidationFunction('birthday', function(string, done) {
 // ## Email
 // Performs some basic sanity checks on email format (see helper method), and
 // uses Kicksend's mailcheck library to offer suggestions for misspellings.
-Validation.registerValidationFunction('email', function(string, done) {
-  if ( isValidEmailSyntax(string) ) {
+Validation.registerValidationFunction("email", function(string, done) {
+  if (isValidEmailSyntax(string)) {
     // We use mailcheck.js to find some common email mispellings
     Mailcheck.run({
       email: string,
@@ -173,14 +174,14 @@ Validation.registerValidationFunction('email', function(string, done) {
       empty: function() {
         return done({
           success: true,
-          message: 'Great, thanks!',
+          message: "Great, thanks!"
         });
       }
     });
   } else {
     return done({
       success: false,
-      message: 'We need a valid email.',
+      message: "We need a valid email."
     });
   }
 });
@@ -188,39 +189,67 @@ Validation.registerValidationFunction('email', function(string, done) {
 // ## Password
 // Checks that password is 6 or more characters long.
 // Matches validation performed in dosomething_user.module.
-Validation.registerValidationFunction('password', function(string, done) {
-  if(string.length >= 6) {
+Validation.registerValidationFunction("password", function(string, done) {
+  if (string.length >= 6) {
     return done({
       success: true,
-      message: 'Keep it secret, keep it safe!',
+      message: "Keep it secret, keep it safe!"
     });
   } else {
     return done({
       success: false,
-      message: 'Must be 6+ characters.',
+      message: "Must be 6+ characters OR ELSE."
     });
   }
 });
 
 // ## Phone
 // Matches validation performed in dosomething_user.module.
-Validation.registerValidationFunction('phone', function(string, done) {
+Validation.registerValidationFunction("phone", function(string, done) {
   // Matches server-side validation from `dosomething_user_valid_cell()` in `dosomething_user.module`.
-  var numberWithoutFormatting = string.replace(/[\-\s\.]/g, '');
-  var isValidFormat = /^(?:\+?([0-9]{1,3})([\-\s\.]{1})?)?\(?([0-9]{3})\)?(?:[\-\s\.]{1})?([0-9]{3})(?:[\-\s\.]{1})?([0-9]{4})/.test(numberWithoutFormatting);
+  var numberWithoutFormatting = string.replace(/[\-\s\.]/g, "");
+  var isValidFormat = /^(?:\+?([0-9]{1,3})([\-\s\.]{1})?)?\(?([0-9]{3})\)?(?:[\-\s\.]{1})?([0-9]{3})(?:[\-\s\.]{1})?([0-9]{4})/.test(
+    numberWithoutFormatting
+  );
 
-  var sanitizedNumber = string.replace(/[^0-9]/g, '');
+  var sanitizedNumber = string.replace(/[^0-9]/g, "");
   var allRepeatingDigits = /([0-9]{1})\1{9,}/.test(sanitizedNumber);
 
-  if(isValidFormat && !allRepeatingDigits) {
+  if (isValidFormat && !allRepeatingDigits) {
     return done({
       success: true,
-      message: 'Thanks!',
+      message: "Thanks!"
     });
   } else {
     return done({
       success: false,
-      message: 'Enter a valid telephone number.',
+      message: "Enter a valid telephone number."
     });
   }
+});
+
+// ## Voter Registration Status
+// Checks that field is filled out.
+Validation.registerValidationFunction("voter_registration_status", function(
+  string,
+  done
+) {
+  console.log("validating voter reg");
+  console.log(string);
+  // console.log(string);
+  // console.log(done);
+  if (
+    string !== "confirmed" ||
+    string !== "unregistered" ||
+    string !== "uncertain"
+  ) {
+    return done({
+      success: false,
+      message: "Let us know if you are registered to vote."
+    });
+  }
+  return done({
+    success: true,
+    message: "Thanks!"
+  });
 });
