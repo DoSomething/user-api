acbbfbe5f7fd23be571dd848c9c3142c238d36b2
katiecrane <kcrane@dosomething.org>
2019-01-03 15:36:07 -0800

    Re-work console command to use queue jobs

--- app/Console/Commands/ImportSubStatusFromCio.php
+++ app/Console/Commands/ImportSubStatusFromCio.php
@@ -2,7 +2,6 @@
 
 namespace Northstar\Console\Commands;
 
-use Carbon\Carbon;
 use Northstar\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
@@ -14,15 +13,14 @@ class ImportSubStatusFromCio extends Command
      *
      * @var string
      */
-    protected $signature = 'northstar:importsub
-                            {--beforeDate= : Only pull updates for users whose status was last updated before this date.}';
+    protected $signature = 'northstar:importsub';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Grab email subscription status from Customer.io for users on which it is not already set.';
+    protected $description = 'For each user, kick off a job to grab email subscription status from Customer.io.';
 
     /**
      * Execute the console command.
@@ -31,59 +29,16 @@ class ImportSubStatusFromCio extends Command
      */
     public function handle()
     {
-        $this->line('Starting up...');
-
-        // Create a Guzzle Client to use with the Customer.io Beta API
-        $client = new \GuzzleHttp\Client([
-            'base_uri' => 'https://beta-api.customer.io',
-        ]);
-
-        // Customer.io authentication
-        $auth = [config('services.customerio.username'), config('services.customerio.password')];
-
-        // Create a query to grab users who have email addresses
         $query = (new User)->newQuery();
-        $query = $query->where('email', 'exists', true);
 
-        // See if the --beforeDate option was used and convert it from a string to a date
-        $date = $this->option('beforeDate');
-        $date = new Carbon($date);
+        $query->chunkById(200, function (Collection $users){
+            $users->each(function (User $user){
+                $queue = config('queue.names.backfill');
 
-        // Iterate through all the users and do the things!
-        $query->chunkById(200, function (Collection $users) use ($client, $auth, $date) {
-            $users->each(function (User $user) use ($client, $auth, $date) {
-                if ($date) {
-                    // Grab when the user's subscription status was last updated and convert from a string to a date
-                    $userUpdated = $user->audit['email_frequency']['updated_at']['date'];
-                    $lastUpdated = new Carbon($userUpdated);
-
-                    // Do not update this user if they were last updated after the specified date
-                    if ($lastUpdated > $date) {
-                        return false;
-                    }
-                }
-
-                // Make request to c.io to get that user's subscription status
-                $response = $client->get('/v1/api/customers/'.$user->id.'/attributes', ['auth' => $auth]);
-                $body = json_decode($response->getBody());
-                $unsubscribed = $body->customer->unsubscribed;
-
-                // Update subscription status on user
-                if ($unsubscribed) {
-                    $user->email_frequency = 'none';
-                }
-                if (! $unsubscribed) {
-                    $user->email_frequency = 'active';
-                }
-                $user->save();
-
-                $this->line('Updated user: '.$user->id);
-
-                // Make sure we don't go over 10 requests per second
-                sleep(.1);
+                dispatch(new GetEmailSubStatusFromCustomerIo($user))->onQueue($queue);
             });
         });
 
-        $this->line('Done!');
+        $this->info('Queued up a job to grab email status for each user!');
     }
 }
