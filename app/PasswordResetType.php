2588462e18ce4a4d50cec8e66db108f511d995d7
Aaron Schachter <aaronschachter@gmail.com>
2019-03-19 10:45:45 -0700

    Refactors CTA Email payload to only pass type

--- app/PasswordResetType.php
+++ app/PasswordResetType.php
@@ -27,31 +27,4 @@ class PasswordResetType
     {
         return [self::$forgotPassword, self::$rockTheVoteActivateAccount];
     }
-
-    /**
-     * Returns Call To Action Email parameters for a given Password Reset Type.
-     *
-     * @param string $type
-     * @return array
-     */
-    public static function getEmailVars($type)
-    {
-        if ($type === self::$rockTheVoteActivateAccount) {
-            return [
-                'actionText' =>'Set Password',
-                'greeting' => 'Hello!',
-                'intro' => 'You are receiving this email because you need to set a password to activate your DoSomething.org account. Here is the link to set your password:',
-                'outro' => 'This link will expire in 24 hours. Once you click the button above, you will be asked to reset your password on the page.<br /><br />If you have further questions, please reach out to help@dosomething.org.',
-                'subject' => 'Activate your DoSomething.org Account',
-            ];
-        }
-
-        return [
-            'actionText' => 'Reset Password',
-            'greeting' => 'Hello!',
-            'intro' => 'You are receiving this email because we received a password reset request for your DoSomething.org account. Here is the link to reset your password:',
-            'outro' => 'This link will expire in 24 hours. Once you click the button above, you will be asked to reset your password on the page. If you did not request a password reset, you can ignore this email. Your password will not change and your account is safe.<br /><br />If you have further questions, please reach out to help@dosomething.org.',
-            'subject' => 'Reset Password',
-        ];
-    }
 }
