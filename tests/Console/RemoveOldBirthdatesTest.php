a5140adbeffbb91c1b759b307579fa5b5790b1d5
David Furnes <david@dfurnes.com>
2019-04-23 11:11:03 -0400

    Remove birthdates before 1900.

---
+++ tests/Console/RemoveOldBirthdatesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Carbon\Carbon;
+use Northstar\Models\User;
+
+class StandardizeBirthdatesTest extends TestCase
+{
+    /** @test */
+    public function it_should_fix_birthdates()
+    {
+        // Create some normal and *really* old users:
+        $normalUsers = factory(User::class, 5)->create();
+        $ancientOnes = factory(User::class, 3)->create(['birthdate' => new Carbon('0001-01-01 00:00:00')]);
+
+        // Run the script!
+        $this->artisan('northstar:olds');
+
+        // Make sure we have 0 borked users
+        foreach ($normalUsers as $user) {
+            $attributes = $user->fresh()->getAttributes();
+            $this->assertArrayHasKey('birthdate', $user->fresh()->getAttributes(), 'Valid birthdates should not be removed.');
+        }
+
+        foreach ($ancientOnes as $user) {
+            $attributes = $user->fresh()->getAttributes();
+            $this->assertArrayNotHasKey('birthdate', $attributes, 'Invalid birthdates should be removed.');
+        }
+    }
+}
