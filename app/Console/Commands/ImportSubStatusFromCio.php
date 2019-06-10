5b14fa4624912489a4b8720d1564571a6b269745
Chloe Lee <chloealessandra@gmail.com>
2019-06-10 14:43:55 -0400

    makes -gray local variable

--- app/Console/Commands/ImportSubStatusFromCio.php
+++ app/Console/Commands/ImportSubStatusFromCio.php
@@ -2,6 +2,7 @@
 
 namespace Northstar\Console\Commands;
 
+use League\Csv\Reader;
 use Northstar\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
@@ -9,53 +10,58 @@ use Northstar\Jobs\GetEmailSubStatusFromCustomerIo;
 
 class ImportSubStatusFromCio extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'northstar:importsub';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'northstar:importsub {--path=}';
 
-    /**
-     * The number of jobs queued up so far.
-     *
-     * @var string
-     */
-    protected $currentCount = 0;
+  /**
+   * The number of jobs queued up so far.
+   *
+   * @var string
+   */
+  protected $currentCount = 0;
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'For each user, kick off a job to grab email subscription status from Customer.io.';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'For each user (with the option to specify users in a given CSV), kick off a job to grab email subscription status from Customer.io.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        // Grab users who have email addresses
-        $query = (new User)->newQuery();
-        $query = $query->where('email', 'exists', true);
+  /**
+   * Execute the console command.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    // If given a CSV, only import emails from CSV.
+    if ($this->argument('path')) {
 
-        $totalCount = $query->count();
+    } else {
+      // Grab users who have email addresses
+      $query = (new User)->newQuery();
+      $query = $query->where('email', 'exists', true);
 
-        $query->chunkById(200, function (Collection $users) use ($totalCount) {
-            $users->each(function (User $user) use ($totalCount) {
-                $queue = config('queue.names.low');
+      $totalCount = $query->count();
 
-                dispatch(new GetEmailSubStatusFromCustomerIo($user))->onQueue($queue);
-            });
+      $query->chunkById(200, function (Collection $users) use ($totalCount) {
+          $users->each(function (User $user) use ($totalCount) {
+              $queue = config('queue.names.low');
 
-            // Logging to track progress
-            $this->currentCount += 200;
-            $percentDone = ($this->currentCount / $totalCount) * 100;
-            $this->line('northstar:importsub - '.$this->currentCount.'/'.$totalCount.' - '.$percentDone.'% done');
-        });
+              dispatch(new GetEmailSubStatusFromCustomerIo($user))->onQueue($queue);
+          });
 
-        $this->line('northstar:importsub - Queued up a job to grab email status for each user!');
+          // Logging to track progress
+          $this->currentCount += 200;
+          $percentDone = ($this->currentCount / $totalCount) * 100;
+          $this->line('northstar:importsub - '.$this->currentCount.'/'.$totalCount.' - '.$percentDone.'% done');
+      });
+
+      $this->line('northstar:importsub - Queued up a job to grab email status for each user!');
     }
+  }
 }
