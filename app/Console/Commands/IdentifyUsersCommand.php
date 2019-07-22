c9623889e9d77d70ce2fe457e6bf88c760541c48
David Furnes <david@dfurnes.com>
2019-07-22 17:47:54 -0400

    Add 'northstar:id' command.

---
+++ app/Console/Commands/IdentifyUsersCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Northstar\Console\Commands;
+
+use League\Csv\Reader;
+use League\Csv\Writer;
+use Northstar\Auth\Registrar;
+use Illuminate\Console\Command;
+
+class IdentifyUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'northstar:id {column=email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get IDs, given a CSV with column that uniquely identifies users.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(Registrar $registrar)
+    {
+        $column = $this->argument('column');
+        $output = Writer::createFromString();
+        $output->insertOne([$column, 'id']);
+
+        $stdin = file_get_contents('php://stdin');
+        $csv = Reader::createFromString($stdin);
+        $csv->setHeaderOffset(0);
+
+        $count = 0;
+        foreach ($csv->getRecords() as $record) {
+            $user = $registrar->resolve([$column => $record[$column]]);
+            $id = $user ? $user->id : '-';
+
+            $output->insertOne([$record[$column], $id]);
+        }
+
+        $this->line($output->getContent());
+    }
+}
