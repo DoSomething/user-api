475e4067ce321a968c7e0462046b46dab19e1051
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-13 13:45:02 -0400

    Add @TODO to limit cover image to current register form

--- resources/views/layouts/cover_image.blade.php
+++ resources/views/layouts/cover_image.blade.php
@@ -1,3 +1,4 @@
+{{-- @TODO: This should be limited to the current register form --}}
 @if (isset($extended) && $extended)
     <div class="cover-image" style="background-image: url({{ session('coverImage', asset('members.jpg')) }})"></div>
 @endif
