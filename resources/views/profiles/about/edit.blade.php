9f6397370d9b28f911f31691ec451e66e95e8932
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-13 12:24:47 -0400

    Add @endsections

--- resources/views/profiles/about/edit.blade.php
+++ resources/views/profiles/about/edit.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.profile_edit')
 
 {{-- @section('title', 'Edit Profile | DoSomething.org') --}}
-<div>
-    @section('profile-form-image')
-        <img src="https://picsum.photos/200/300"/>
-    @section('profile-title')
-        <h2>A Title!</h2>
-    @section('profile-subtitle')
-        <p>A subtitle!<p>
-    @section('profile-form')
-        <form>
-            <div class="form-item">
-                <label for="first-name" class="field-label">First Name</label>
-                <input type="text" id="first-name" class="text-field" name="first_name" value="{{ old('first_name') ?: $user->first_name }}" autofocus />
-            </div>
-        </form>
-<div>
+
+@section('profile-form-image')
+    <img src="https://picsum.photos/200/300"/>
+@endsection
+
+@section('profile-title')
+    <h2>A Title!</h2>
+@endsection
+@section('profile-subtitle')
+    <p>A subtitle!<p>
+@endsection
+
+@section('profile-form')
+    <form>
+        <div class="form-item">
+            <label for="first-name" class="field-label">First Name</label>
+            <input type="text" id="first-name" class="text-field" name="first_name" value="{{ old('first_name') ?: $user->first_name }}" autofocus />
+        </div>
+    </form>
+@endsection
