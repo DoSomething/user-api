2b2b19d1672d758ed0541eaa8ffdefe4407f1b09
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-09-17 15:17:23 -0400

    updates profile template with a few main css adds

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
+{{-- @TODO: update padding of container --}}
 @section('content')
-    <div style="background-image: url(@yield('form-image-url'))">
-        @yield('profile-title')
-        @yield('profile-subtitle')
-        @yield('profile-form')
+    <div class="pt-12 md:pt-24 lg:pt-0 lg:pl-24 " style="background-image: url(@yield('form-image-url'))">
+        <div class=" pl-6 bg-white font-source-sans ">
+            @yield('profile-title')
+            @yield('profile-subtitle')
+            @yield('profile-form')
+        </div>
     </div>
 @endsection
