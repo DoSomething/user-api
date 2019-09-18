caf7d90bed24fe89b91749a2660e115a14c19abf
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-18 14:50:14 -0400

    Render cover image only if $coverImage var is truthy
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>

--- resources/views/layouts/cover_image.blade.php
+++ resources/views/layouts/cover_image.blade.php
@@ -1,4 +1,4 @@
 {{-- @TODO: This should be limited to the current register form --}}
-@if (isset($extended) && $extended)
+@if (isset($coverImage) && $coverImage)
     <div class="cover-image" style="background-image: url({{ session('coverImage', asset('members.jpg')) }})"></div>
 @endif
