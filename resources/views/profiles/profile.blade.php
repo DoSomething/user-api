2e227d80e8304fe35ecfcf55252d90907551dc66
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-18 14:51:41 -0400

    Update padding for profile form content
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app', ['extended' => true])
 
-{{-- @TODO: update padding of container --}}
 @section('content')
+    {{-- @TODO: add percentage based width to the tailwind config & adjust lg:pl-24 to 1/3 --}}
     <div class="pt-12 md:pt-24 lg:pt-0 lg:pl-24 " style="background-image: url(@yield('form-image-url'))">
-        <div class=" pl-6 bg-white font-source-sans ">
+        <div class=" p-6 bg-white font-source-sans ">
             @yield('profile-title')
             @yield('profile-subtitle')
             @yield('profile-form')
