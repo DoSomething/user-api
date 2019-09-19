c48379da35f23009fe06d96d6580014da8119e9a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-09-19 11:11:27 -0400

    Remove TODO comment

--- resources/views/layouts/cover_image.blade.php
+++ resources/views/layouts/cover_image.blade.php
@@ -1,4 +1,3 @@
-{{-- @TODO: This should be limited to the current register form --}}
 @if (isset($coverImage) && $coverImage)
     <div class="cover-image" style="background-image: url({{ session('coverImage', asset('members.jpg')) }})"></div>
 @endif
