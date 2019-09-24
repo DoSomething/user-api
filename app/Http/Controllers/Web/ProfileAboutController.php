068dbd7887898e13a278487f7a9cd2a5f408abb9
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-09-24 14:48:18 -0400

    adds validation and correct formatting of data for form submissions

--- app/Http/Controllers/Web/ProfileAboutController.php
+++ app/Http/Controllers/Web/ProfileAboutController.php
@@ -4,6 +4,7 @@ namespace Northstar\Http\Controllers\Web;
 
 // use Northstar\Models\User;
 use Illuminate\Http\Request;
+use Northstar\Auth\Registrar;
 use Northstar\Http\Controllers\Controller;
 use Northstar\Http\Requests\ProfileAboutRequest;
 
@@ -12,8 +13,9 @@ class ProfileAboutController extends Controller
     /**
      * Add Authentication Middleware.
      */
-    public function __construct()
+    public function __construct(Registrar $registrar)
     {
+        $this->registrar = $registrar;
         $this->middleware('auth:web');
         $this->middleware('role:admin,staff');
     }
@@ -43,24 +45,29 @@ class ProfileAboutController extends Controller
         // write a foreach with the causes and push them into causes if it already exists
         // save the user
         $user = auth()->guard('web')->user();
-        $profile_input = $request->all();
-        $input_causes = array_slice($profile_input, 6);
-        // $birthdate = $profile_input["month"]."/".$profile_input["day"]."/".$profile_input["year"];
 
-        if($profile_input["birthdate"])
-            $user->birthdate = $request->birthdate;
+        
+        $request["birthdate"] = implode("/", $request["birthdate"]);
 
-        if($profile_input["voter_registration_status"])
-            $user->voter_registration_status = $profile_input["voter_registration_status"];
+        if(strlen($request["birthdate"] < 3))
+            $request["birthdate"] = null;
 
-        if($input_causes)
-                $user->causes = array_merge($user->causes, $input_causes);
-
-        dd([
-            $request,
-            $request->all(),
-            auth()->guard('web')->user()
+        $this->registrar->validate($request, null, [
+            'birthdate' => 'nullable|date|date:now',
+            'email' => 'email|nullable|unique:users',
+            'mobile' => 'mobile|nullable|unique:users',
         ]);
+
+        if($request["birthdate"])
+            $user->birthdate = $request["birthdate"];
+
+        if($request["voter_registration_status"])
+            $user->voter_registration_status = $request["voter_registration_status"];
+
+        if($request["causes"])
+            $user->causes = array_merge($user->causes, $request["causes"]);
+
+        $user->save();
 
         return redirect(url('profile/subscriptions'));
     }
--- resources/views/profiles/about/edit.blade.php
+++ resources/views/profiles/about/edit.blade.php
@@ -122,4 +122,14 @@
         </div>
         
     </form>
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 @endsection
