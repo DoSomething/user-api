d27d1e6e48bc417b5b947f945b6952fbb694a319
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-10-01 17:04:42 -0400

    updates tailwind config to include padding percentages, updates reg templates with tw and creates an images folder for all the images in northstar

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     {{-- @TODO: add percentage based width to the tailwind config & adjust lg:pl-24 to 1/3 --}}
-    <div class="pt-12 md:pt-24 lg:pt-0 lg:pl-24 " style="background-image: url(@yield('form-image-url'))">
+    <div class="pt-20 md:pt-1/4 lg:pt-0 lg:pl-1/3 " style="background-image: url(@yield('form-image-url'))">
         <div class=" p-6 bg-white font-source-sans ">
             @yield('profile-title')
             @yield('profile-subtitle')
