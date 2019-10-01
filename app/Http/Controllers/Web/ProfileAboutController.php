aec063ebc18a5719029b890c6c4647bfbfbe010f
Lindsay Maher <lindsaymah3r@gmail.com>
2019-10-01 14:08:38 -0400

    About controller validation (#917)
    * stubs out basic steps for profile pages
    
    * adds starting skeleton for reg pages
    
    * Add @endsections
    
    * Re-add title @section to profiles/about/edit form
    
    * Use 'extended' setting for full width
    
    * Add @TODO to limit cover image to current register form
    
    * Updates for linter & some todos comments
    
    * Add Profile Subscriptions route/controller/view
    
    * updates packages
    
    * updates about form and tests using cover image blade template
    
    * adds mock buttons on about form
    
    * renames and moves profile blade file
    
    * starts adding base styling to profile template
    
    * updates profile template with a few main css adds
    
    * adds padding back to container
    
    * updates with doc blocks for about controller, matches blade templates and adds post routes
    
    * style changes
    
    * more styling changes
    
    * Append -cover-image class set through $coverImage var
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Render cover image only if $coverImage var is truthy
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Limit .wrapper.-half width only for -cover-image
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Set $coverImage var for registration form
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Set $extended var for profile forms
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Update padding for profile form content
    
    Co-Authored-By: Lindsay Maher <lindsayrmaher@users.noreply.github.com>
    
    * Update routes comment
    
    * Fix typo in controller definition
    
    * Use patch request for profile update routes
    
    * Add DocBlocks for ProfileSubscriptionsController
    
    * Remove TODO comment
    
    * change store method to update in about and subscription files, as well as routes
    
    * adds cause areas to form, updates copy in some form fields
    
    * separates birthday into month, day and year
    
    * adds next and skip buttons
    
    * investigating patch action
    
    * fixed the form submit!
    
    * add request in update method for about controller
    
    * adding comments for monday morning work
    
    * updates todos and cleans code for PR
    
    * spacing updates
    
    * adds good amount of validation start
    
    * updates form to group birthdate and causes in request
    
    * adds validation and correct formatting of data for form submissions
    
    * removes todos
    
    * removes unused code
    
    * removes reference to form request that has been deleted
    
    * removes more references to unused form request
    
    * removes error messages
    
    * styling changes
    
    * styling changes
    
    * styling changes
    
    * styling changes
    
    * adds maxlength on birthday fields
    
    * adds comment to explain date check, updates to before:now and removes unnecessary bracketts
    
    * updates to use the fill method and remove unnecessary conditionals. also updates logic not to include merging the current causes with newly selected
    
    * creates a partial for the checkbox field in a form, loops through set of causes to fill in the partials
    
    * spacing updates
    
    * removes quotes from form labels
    
    * adds spacing in arrays
    
    * add trailing commas

--- app/Http/Controllers/Web/ProfileAboutController.php
+++ app/Http/Controllers/Web/ProfileAboutController.php
@@ -2,8 +2,8 @@
 
 namespace Northstar\Http\Controllers\Web;
 
-// use Northstar\Models\User;
 use Illuminate\Http\Request;
+use Northstar\Auth\Registrar;
 use Northstar\Http\Controllers\Controller;
 
 class ProfileAboutController extends Controller
@@ -11,20 +11,36 @@ class ProfileAboutController extends Controller
     /**
      * Add Authentication Middleware.
      */
-    public function __construct()
+    public function __construct(Registrar $registrar)
     {
+        $this->registrar = $registrar;
         $this->middleware('auth:web');
         $this->middleware('role:admin,staff');
+        $this->causes = [
+            'animal_welfare'=> 'Animal Welfare',
+            'bullying'=>'Bullying',
+            'education'=>'Education',
+            'environment' => 'Environment',
+            'gender_rights_equality' => 'Gender Rights & Equality',
+            'homelessness_poverty'=> 'Homelessness & Poverty',
+            'immigration_refugees'=> 'Immigration & Refugees',
+            'lgbtq_rights_equality' => 'LGBTQ+ Rights & Equality',
+            'mental_health' => 'Mental Health',
+            'physical_health' => 'Physical Health',
+            'racial_justice_equity' => 'Racial Justice & Equity',
+            'sexual_harassment_assault' => 'Sexual Harassment & Assault',
+        ];
     }
 
     /**
      * Display the User Details Form
-     *
-     * @return \Illuminate\Http\Response
      */
     public function edit()
     {
-        return view('profiles.about.edit', ['user' => auth()->guard('web')->user()]);
+        return view('profiles.about.edit', [
+            'user' => auth()->guard('web')->user(),
+            'causes' => $this->causes,
+            ]);
     }
 
     /**
@@ -35,22 +51,27 @@ class ProfileAboutController extends Controller
      */
     public function update(Request $request)
     {
-        // @TODO: finish adding validation and appending data to the user
-        // notes for monday:
-        // concantenate dates together into a single string & validate
-        // add conditionals for whether there is a value passed from the user for each field
-        // write a foreach with the causes and push them into causes if it already exists
-        // save the user
-
-        $input = $request->all();
-        //user here - use to update and validate specific attributes
         $user = auth()->guard('web')->user();
 
-        // dd([
-        //     $request,
-        //     $request->all(),
-        //     auth()->guard('web')->user()
-        // ]);
+        //creates a single string from the 3 dates inputted by the user
+        $request['birthdate'] = implode('/', $request['birthdate']);
+
+        //checks if the birthdate we create above contains an inputted date
+        if (strlen($request['birthdate'] < 3)) {
+            $request['birthdate'] = null;
+        }
+
+        $this->registrar->validate($request, null, [
+            'birthdate' => 'required|date|before:now',
+            'email' => 'email|nullable|unique:users',
+            'mobile' => 'mobile|nullable|unique:users',
+        ]);
+
+        $completedFields = array_filter($request->all());
+
+        $user->fill($completedFields);
+
+        $user->save();
 
         return redirect(url('profile/subscriptions'));
     }
--- resources/views/profiles/about/edit.blade.php
+++ resources/views/profiles/about/edit.blade.php
@@ -22,20 +22,20 @@
             <label for="birthdate" class="field-label">{{ trans('auth.fields.birthday') }}</label>
             <div class="form-item -reduced">
                 <label for="month">Month</label>
-                <input class="text-field" name="month" type="text" placeholder="MM" />
+                <input class="text-field" name="birthdate[]" type="text" placeholder="MM" maxlength="2"  />
             </div>
             <div class="form-item -reduced">
                 <label for="day">Day</label>
-                <input class="text-field" name="day" type="text" placeholder="DD" />
+                <input class="text-field" name="birthdate[]" type="text" placeholder="DD" maxlength="2" />
             </div>
             <div class="form-item -reduced">
                 <label for="year">Year</label>
-                <input class="text-field" name="year" type="text" placeholder="YYYY" />
+                <input class="text-field" name="birthdate[]" type="text" placeholder="YYYY" maxlength="4" />
             </div>
         </div>
 
         <div class="form-item">
-            <label for="voter_registration_status" class="field-label height-auto">{{ "Are you registered to vote at your current address?"}}</label>
+            <label for="voter_registration_status" class="field-label height-auto">Are you registered to vote at your current address?</label>
             <div class="form-item -reduced">
                 <label class="option -radio">
                     <input type="radio" name="voter_registration_status" value="confirmed" {{ old('voter_registration_status') === 'confirmed' ? 'checked' : '' }}>
@@ -59,58 +59,13 @@
             </div>
         </div>
 
-        {{-- @TODO: Refactor to loop through list of causes to create the different options --}}
         <div class="form-item">
-            <label for="cause_areas" class="field-label height-auto">{{"What cause areas do you care about most?"}}</label>
-
-            <div>
-                <input type="checkbox" name="animal_welfare" value="animal_welfare">
-                <span>Animal Welfare</span>
-            </div>
-            <div>
-                <input type="checkbox" name="bullying" value="bullying">
-                <span>Bullying</span>
-            </div>
-            <div>
-                <input type="checkbox" name="education" value="education">
-                <span>Education</span>
-            </div>
-            <div>
-                <input type="checkbox" name="environment" value="environment">
-                <span>Environment</span>
-            </div>
-            <div>
-                <input type="checkbox" name="gender_rights_equality" value="gender_rights_equality">
-                <span>Gender Rights & Equality</span>
-            </div>
-            <div>
-                <input type="checkbox" name="homelessness_poverty" value="homelessness_poverty">
-                <span>Homelessness & Poverty</span>
-            </div>
-            <div>
-                <input type="checkbox" name="immigration_refugees" value="immigration_refugees">
-                <span>Immigration & Refugees</span>
-            </div>
-            <div>
-                <input type="checkbox" name="lgbtq_rights_equality" value="lgbtq_rights_equality">
-                <span>LGBTQ+ Rights & Equality</span>
-            </div>
-            <div>
-                <input type="checkbox" name="mental_health" value="mental_health">
-                <span>Mental Health</span>
-            </div>
-            <div>
-                <input type="checkbox" name="physical_health" value="physical_health">
-                <span>Physical Health</span>
-            </div>
-             <div>
-                <input type="checkbox" name="racial_justice_equity" value="racial_justice_equity">
-                <span>Racial Justice & Equity</span>
-            </div>
-             <div>
-                <input type="checkbox" name="sexual_harassment_assault" value="sexual_harassment_assault">
-                <span>Sexual Harassment & Assault</span>
-            </div>
+            <label for="cause_areas" class="field-label height-auto">What cause areas do you care about most?</label>
+            <fieldset>
+                    @foreach ( $causes as $value => $label )
+                            @include('forms.checkbox', ['name' => 'causes[]', 'value' => $value , 'label' =>  $label])
+                    @endforeach
+            </fieldset>
         </div>
 
         <div class="form-actions -padded -left">
