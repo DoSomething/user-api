65f7a1c6dc470fd67d8b19d7500b3c675247b7fb
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-10-08 12:09:59 -0500

    fixes labeling issue for checkbox styles

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,12 +1,12 @@
-<div>
+{{-- <div>
     <input type="checkbox" name="{{ $name }}[]" value="{{ $value }}" {{in_array($value, (count($errors) ? old($name) : $user->{$name}) ?: []) ? "checked" : null}}>
     <span>{{ $label }}</span>
-</div>
+</div> --}}
 
 
-{{-- this one should work but it doesn't... --}}
-{{-- <label for="{{ $value }}" class="option -checkbox">
-    <input type="checkbox" name="{{ $name }}[]" value="{{ $value }}" {{in_array($value, (count($errors) ? old($name) : $user->{$name}) ?: []) ? "checked" : null}}>
+
+<label for="{{ $value }}" class="option -checkbox">
+    <input type="checkbox" name="{{ $name }}[]" id="{{ $value }}" value="{{ $value }}" {{in_array($value, (count($errors) ? old($name) : $user->{$name}) ?: []) ? "checked" : null}}>
     <span class="option__indicator"></span>
     <span>{{ $label }}</span>
-</label> --}}
+</label>
