469d0a9ee311652714baa526c36d52f5e31e0b94
Lindsay Maher <lmaher@Lindsays-MacBook-Pro.local>
2019-10-08 14:55:57 -0500

    removes comment

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,10 +1,3 @@
-{{-- <div>
-    <input type="checkbox" name="{{ $name }}[]" value="{{ $value }}" {{in_array($value, (count($errors) ? old($name) : $user->{$name}) ?: []) ? "checked" : null}}>
-    <span>{{ $label }}</span>
-</div> --}}
-
-
-
 <label for="{{ $value }}" class="option -checkbox">
     <input type="checkbox" name="{{ $name }}[]" id="{{ $value }}" value="{{ $value }}" {{in_array($value, (count($errors) ? old($name) : $user->{$name}) ?: []) ? "checked" : null}}>
     <span class="option__indicator"></span>
