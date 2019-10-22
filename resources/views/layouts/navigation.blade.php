3b222ef053890803d891be82521ebd847c70e67c
katiecrane <kcrane@dosomething.org>
2019-10-22 15:27:16 -0700

    Don't hide logo on mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,4 @@
 <nav class="navigation">
-    <div class="navigation__menu">
-        <a class="navigation__logo" href="http://www.dosomething.org"><span>DoSomething.org</span></a>
-        <a class="navigation__toggle js-navigation-toggle" href="#"><span>Show Menu</span></a>
-    </div>
+    <a class="navigation__logo" href="http://www.dosomething.org"><span>DoSomething.org</span></a>
+    <a class="navigation__toggle js-navigation-toggle" href="#"><span>Show Menu</span></a>
 </nav>
