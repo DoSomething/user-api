fa699cd2a536bfe790b26c9ae0aea344b1e46100
Katie Crane <kcrane@dosomething.org>
2019-10-23 14:52:08 -0700

    Remove nav menu items from Northstar (#947)
    * Remove nav menu items
    
    * Don't hide logo on mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,20 +1,4 @@
 <nav class="navigation">
     <a class="navigation__logo" href="http://www.dosomething.org"><span>DoSomething.org</span></a>
     <a class="navigation__toggle js-navigation-toggle" href="#"><span>Show Menu</span></a>
-    <div class="navigation__menu">
-        <ul class="navigation__primary">
-            <li>
-                <a href="{{ config('services.phoenix.url') }}/campaigns">
-                  <strong class="navigation__title">Explore Campaigns</strong>
-                  <span class="navigation__subtitle">Find ways to take action both online and off.</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ config('services.phoenix.url') }}/about/who-we-are">
-                  <strong class="navigation__title">What Is DoSomething.org?</strong>
-                  <span class="navigation__subtitle">A global movement for good. </span>
-                </a>
-            </li>
-        </ul>
-    </div>
 </nav>
