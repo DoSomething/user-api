850a5daf4e94632747fdd81ff36fd8eddf8b9d20
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-11-22 11:32:05 -0500

    Add new Refer Friends Scholarship ENV variable

--- config/features.php
+++ config/features.php
@@ -22,5 +22,5 @@ return [
 
     'optional-fields' => env('DS_OPTIONAL_FIELDS', false),
 
-    'refer-friends' => env('DS_REFER_FRIENDS_TEST', false),
+    'refer-friends-scholarship' => env('DS_REFER_FRIENDS_SCHOLARSHIP_TEST', false),
 ];
