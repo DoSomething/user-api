91c4141ff0d946d447c70bd577ea2dd8a646f6e7
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-01-24 11:25:19 -0500

    initial changes to google auth button and FB button

--- resources/views/auth/google.blade.php
+++ resources/views/auth/google.blade.php
@@ -3,9 +3,9 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 @endsection
 
-    <a href="{{ url('google/continue') }}" class="button pl-4 google-login" >
-        <div class="hidden md:inline-block">
-            @include('icons.google-icon-2')
+    <a href="{{ url('google/continue') }}" class="button google-login flex justify-start" >
+        <div class="inline-block ml-0 mr-4">
+            @include('icons.new-google-icon')
         </div>
         Continue with Google
     </a>
