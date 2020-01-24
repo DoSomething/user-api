00df9ee70d4f54d443b3da98aade015f73356118
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-01-24 16:36:10 -0500

    updates FB logo position and slight adjustment of labels

--- resources/views/auth/google.blade.php
+++ resources/views/auth/google.blade.php
@@ -3,7 +3,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 @endsection
 
-    <a href="{{ url('google/continue') }}" class="button google-login" >
+    <a href="{{ url('google/continue') }}" class="button google-login align-middle" >
         <div class="inline-block mr-4">
             @include('icons.google-icon-white')
         </div>
