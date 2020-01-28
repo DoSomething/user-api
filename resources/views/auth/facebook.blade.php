7222fb8f0299b69f922dab6a5e35724649029a1b
Lindsay Maher <lindsaymah3r@gmail.com>
2020-01-28 11:16:51 -0500

    vertically aligning button labels (#974)

--- resources/views/auth/facebook.blade.php
+++ resources/views/auth/facebook.blade.php
@@ -1,1 +1,5 @@
-<a href="{{ url('facebook/continue') }}" class="button facebook-login">{{ trans('auth.log_in.facebook') }}</a>
+<a href="{{ url('facebook/continue') }}" class="button facebook-login">
+    <span class="align-middle">
+        {{ trans('auth.log_in.facebook') }}
+    </span>
+</a>
--- resources/views/auth/google.blade.php
+++ resources/views/auth/google.blade.php
@@ -3,9 +3,9 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 @endsection
 
-    <a href="{{ url('google/continue') }}" class="button google-login align-middle" >
+    <a href="{{ url('google/continue') }}" class="button google-login" >
         <div class="inline-block mr-4">
             @include('icons.google-icon-white')
         </div>
-        Continue with Google
+           <span class="align-middle"> Continue with Google</span>
     </a>
