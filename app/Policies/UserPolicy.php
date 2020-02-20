6f2efad3a0e83688a1bf95d9ff676a83dbfb1f1b
David Furnes <david@dfurnes.com>
2020-02-20 15:36:21 -0500

    Refactor UserPolicy for readability.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -11,52 +11,68 @@ class UserPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine if the authorized user can see full profile details
-     * for the given user account.
+     * Is the current user a staffer/administrator?
      *
-     * @param User $user
-     * @param User $profile
+     * @param User $viewer
      * @return bool
      */
-    public function viewFullProfile(?User $user, User $profile)
+    protected function isSuperuser(?User $viewer)
     {
+        // If this is a machine client, it's a "superuser" even
+        // though there isn't actually a _user_ authenticated:
         if (Scope::allows('admin')) {
             return true;
         }
 
-        if (! $user) {
+        // But if this isn't a machine client & anonymous, they
+        // certainly can't be a superuser, now can they?!
+        if (! $viewer) {
             return false;
         }
 
-        if (in_array($user->role, ['admin', 'staff'])) {
-            return true;
+        // Superusers are folks with 'admin' or 'staff' role:
+        return in_array($viewer->role, ['admin', 'staff']);
+    }
+
+    /**
+     * Is the current user the same person as this profile?
+     *
+     * @param User $viewer
+     * @param User $target
+     * @return bool
+     */
+    protected function isOwner(?User $viewer, User $target)
+    {
+        if (! $viewer) {
+            return false;
         }
 
-        return $user->id === $profile->id;
+        return $viewer->is($target);
+    }
+
+    /**
+     * Determine if the authorized user can see full profile details
+     * for the given user account.
+     *
+     * @param User $viewer
+     * @param User $target
+     * @return bool
+     */
+    public function viewFullProfile(?User $viewer, User $target)
+    {
+        return $this->isSuperuser($viewer) || $this->isOwner($viewer, $target);
     }
 
     /**
      * Determine if the authorized user can edit the profile details
      * for the given user account.
      *
-     * @param User $user
-     * @param User $profile
+     * @param User $viewer
+     * @param User $target
      * @return bool
      */
-    public function editProfile(?User $user, User $profile)
+    public function editProfile(?User $viewer, User $target)
     {
-        if (Scope::allows('admin')) {
-            return true;
-        }
-
-        if (! $user) {
-            return false;
-        }
-
-        if (in_array($user->role, ['admin', 'staff'])) {
-            return true;
-        }
-
-        return $user->id === $profile->id;
+        return $this->isSuperuser($viewer) || $this->isOwner($viewer, $target);
     }
 }
