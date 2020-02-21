2b03bc3e8e8ba65742ff682b83c6b2101ce98d0d
David Furnes <david@dfurnes.com>
2020-02-21 16:42:15 -0500

    Add an index on 'deletion_requested_at'. (#985)

---
+++ database/migrations/2020_02_21_212702_AddIndexToDeletionRequestedAt.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToDeletionRequestedAt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $collection) {
+            $collection->index('deletion_requested_at', null, null, ['sparse' => true]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $collection) {
+            $collection->dropIndex('deletion_requested_at');
+        });
+    }
+}
