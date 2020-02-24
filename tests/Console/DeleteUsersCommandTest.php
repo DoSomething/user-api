f2f455ae117a47881db127e5f6e125ab82d1d265
David Furnes <david@dfurnes.com>
2020-02-24 11:58:08 -0500

    Immediately delete users when running 'northstar:delete' command. (#986)
    * The 'northstar:delete' command should immediately delete.
    
    * Update command documentation.

--- tests/Console/DeleteUsersCommandTest.php
+++ tests/Console/DeleteUsersCommandTest.php
@@ -1,6 +1,9 @@
 <?php
 
 use Northstar\Models\User;
+use Northstar\Services\Rogue;
+use Northstar\Services\Gambit;
+use Northstar\Services\CustomerIo;
 
 class DeleteUsersCommandTest extends TestCase
 {
@@ -14,11 +17,16 @@ class DeleteUsersCommandTest extends TestCase
         $user1 = factory(User::class)->create(['_id' => '5d3630a0fdce2742ff6c64d4'])->first();
         $user2 = factory(User::class)->create(['_id' => '5d3630a0fdce2742ff6c64d5'])->first();
 
+        // Mock the external service APIs & assert that we make two "delete" requests:
+        $this->mock(Rogue::class)->shouldReceive('deleteUser')->twice();
+        $this->mock(Gambit::class)->shouldReceive('deleteUser')->twice();
+        $this->mock(CustomerIo::class)->shouldReceive('deleteUser')->twice();
+
         // Run the 'northstar:delete' command on the 'example-identify-output.csv' file:
         $this->artisan('northstar:delete', ['input' => $input]);
 
         // The command should remove
-        $this->assertEquals($user1->fresh()->deletion_requested_at, $now);
-        $this->assertEquals($user2->fresh()->deletion_requested_at, $now);
+        $this->assertAnonymized($user1);
+        $this->assertAnonymized($user2);
     }
 }
--- tests/Console/ProcessDeletionsCommandTest.php
+++ tests/Console/ProcessDeletionsCommandTest.php
@@ -33,33 +33,4 @@ class ProcessDeletionsCommandTest extends TestCase
         $this->assertArrayNotHasKey('deleted_at', $user3->fresh()->getAttributes());
         $this->assertArrayNotHasKey('deleted_at', $user4->fresh()->getAttributes());
     }
-
-    /**
-     * Assert that the given model has been anonymized.
-     *
-     * @param User $before
-     */
-    protected function assertAnonymized($before)
-    {
-        $after = $before->fresh();
-        $attributes = $after->getAttributes();
-
-        // The birthdate should be set to January 1st of the same year:
-        $this->assertEquals($before->birthdate->year, $after->birthdate->year);
-        $this->assertEquals(1, $after->birthdate->month);
-        $this->assertEquals(1, $after->birthdate->day);
-
-        // We should not see any fields with PII:
-        $this->assertArrayNotHasKey('email', $attributes);
-        $this->assertArrayNotHasKey('first_name', $attributes);
-        $this->assertArrayNotHasKey('last_name', $attributes);
-        $this->assertArrayNotHasKey('addr_street1', $attributes);
-        $this->assertArrayNotHasKey('addr_street2', $attributes);
-
-        // ...but we should still have some demographic fields:
-        $this->assertArrayHasKey('addr_zip', $attributes);
-
-        // We should also have set a "deleted at" flag:
-        $this->assertArrayHasKey('deleted_at', $attributes);
-    }
 }
