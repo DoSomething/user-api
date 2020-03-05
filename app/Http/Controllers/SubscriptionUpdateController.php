1e352747ee689adbcc1969fc56687b1cdea4c684
Luke Patton <lpatton@dosomething.org>
2020-03-05 10:02:47 -0500

    Email subscription resource (#990)
    * New controller and routes for adding and removing individual email subscriptions
    
    There's both an add and remove method available that operate on the passed in topic
    
    * Rename REST method to store
    
    * Add unit tests
    
    Testing for:
    - Adding new subscriptions
    - Not duplicating subscriptions in the array
    - Removing subscriptions
    - Authorizing proper users to update subscriptions
    
    * Fixes some style issues and adds better test descriptions
    
    * More styleCI fixes
    
    * StyleCI never happy
    
    * Like literally a single line break

---
+++ app/Http/Controllers/SubscriptionUpdateController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Northstar\Http\Controllers;
+
+use Northstar\Models\User;
+use Northstar\Http\Transformers\UserTransformer;
+
+class SubscriptionUpdateController extends Controller
+{
+    /**
+     * @var UserTransformer
+     */
+    protected $transformer;
+
+    /**
+     * Make a new SubpscriptionUpdateController, inject dependencies,
+     * and set middleware for this controller's methods.
+     *
+     * @param UserTransformer $transformer
+     */
+    public function __construct(UserTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+
+        $this->middleware('scope:user');
+        $this->middleware('scope:write');
+    }
+
+    public function store(User $user, string $topic)
+    {
+        $this->authorize('edit-profile', $user);
+
+        if (! in_array($topic, ['news', 'scholarships', 'community', 'lifestyle'])) {
+            abort(404, 'That subscription does not exist.');
+        }
+
+        $user->push('email_subscription_topics', $topic, true);
+
+        return $this->item($user);
+    }
+
+    public function destroy(User $user, string $topic)
+    {
+        $this->authorize('edit-profile', $user);
+
+        if (! in_array($topic, ['news', 'scholarships', 'community', 'lifestyle'])) {
+            abort(404, 'That subscription does not exist.');
+        }
+
+        $user->pull('email_subscription_topics', $topic);
+
+        return $this->item($user);
+    }
+}
---
+++ tests/Http/SubscriptionUpdateTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use Northstar\Models\User;
+
+class SubscriptionUpdateTest extends BrowserKitTestCase
+{
+    /**
+     * Test that a user can add email subscriptions.
+     * POST /v2/users/:id/subscriptions/:topic
+     *
+     * @return void
+     */
+    public function testAddSubscription()
+    {
+        $user = factory(User::class)->create();
+
+        $this->asUser($user, ['user', 'write'])->post('v2/users/'.$user->id.'/subscriptions/news');
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonField('data.email_subscription_topics', ['news']);
+    }
+
+    /**
+     * Test that a user cannot add a duplicate email subscription.
+     * POST /v2/users/:id/subscriptions/:topic
+     *
+     * @return void
+     */
+    public function testAddExistingSubscription()
+    {
+        $user = factory(User::class)->create([
+            'email_subscription_topics' => ['news'],
+        ]);
+
+        $this->asUser($user, ['user', 'write'])->post('v2/users/'.$user->id.'/subscriptions/news');
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonField('data.email_subscription_topics', ['news']);
+    }
+
+    /**
+     * Test that a user can mark remove email subscriptions.
+     * DELETE /v2/users/:id/subscriptions/:topic
+     *
+     * @return void
+     */
+    public function testRemoveSubscription()
+    {
+        $user = factory(User::class)->create([
+            'email_subscription_topics' => ['news'],
+        ]);
+
+        $this->asUser($user, ['user', 'write'])->delete('v2/users/'.$user->id.'/subscriptions/news');
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonField('data.email_subscription_topics', []);
+    }
+
+    /**
+     * Test that a user can't edit another user's subscriptions.
+     * POST /v2/users/:id/subscriptions/:topic
+     *
+     * @return void
+     */
+    public function testNormalUsersCantChangeOthersSubscriptions()
+    {
+        $villain = factory(User::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->asUser($villain, ['user', 'write'])->post('v2/users/'.$user->id.'/subscriptions/news');
+
+        $this->assertResponseStatus(403);
+    }
+}
