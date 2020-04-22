5d49978761964435b2e3876f8c7f027d982375a9
Aaron Schachter <aaronschachter@gmail.com>
2020-04-22 06:53:44 -0700

    Adds SMS status argument for SMS subscription topics backfill (#1013)
    * Requires passing SMS status argument
    
    * Rename to smsStatus

--- app/Console/Commands/SetDefaultSmsSubscriptionTopics.php
+++ app/Console/Commands/SetDefaultSmsSubscriptionTopics.php
@@ -13,14 +13,14 @@ class SetDefaultSmsSubscriptionTopics extends Command
      *
      * @var string
      */
-    protected $signature = 'northstar:default-sms-topics';
+    protected $signature = 'northstar:default-sms-topics {smsStatus}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Add "general" and "voting" SMS topics for each user who is subscribed to SMS.';
+    protected $description = 'Add "general" and "voting" SMS topics for each user who does not have topics set and has the given SMS status.';
 
     /**
      * The number of users updated so far.
@@ -46,24 +46,32 @@ class SetDefaultSmsSubscriptionTopics extends Command
      */
     public function handle()
     {
+        $smsStatus = $this->argument('smsStatus');
+
+        if (! in_array($smsStatus, ['active', 'less', 'pending'])) {
+            $this->line('northstar:default-sms-topics - Invalid smsStatus argument "'.$smsStatus.'".');
+
+            return;
+        }
+
         // Use low priority queue for these updates
         config(['queue.jobs.users' => 'low']);
 
-        // Grab users who are subscribers and do not have topics set.
+        // Grab users who have our status argument and do not have topics set.
         $query = (new User)->newQuery();
-        $query = $query->whereIn('sms_status', ['active', 'less', 'pending']);
+        $query = $query->where('sms_status', $smsStatus);
         $query = $query->where('sms_subscription_topics', null);
 
         $totalCount = $query->count();
 
         if ($totalCount === 0) {
-            $this->line('northstar:default-sms-topics - No users need updating.');
+            $this->line('northstar:default-sms-topics - No users with smsStatus "'.$smsStatus.'" need updating.');
 
             return;
         }
 
-        $query->chunkById(200, function (Collection $users) use ($totalCount) {
-            $users->each(function (User $user) use ($totalCount) {
+        $query->chunkById(200, function (Collection $users) use ($totalCount, $smsStatus) {
+            $users->each(function (User $user) use ($totalCount, $smsStatus) {
                 $user->sms_subscription_topics = ['general', 'voting'];
                 $user->save();
             });
@@ -71,9 +79,9 @@ class SetDefaultSmsSubscriptionTopics extends Command
             // Logging to track progress (may read over 100% at the end when there are less than 200 users in the last chunk)
             $this->currentCount += 200;
             $percentDone = ($this->currentCount / $totalCount) * 100;
-            $this->line('northstar:default-sms-topics - status update - '.$this->currentCount.'/'.$totalCount.' - '.$percentDone.'% done');
+            $this->line('northstar:default-sms-topics - smsStatus "'.$smsStatus.'" - '.$this->currentCount.'/'.$totalCount.' - '.$percentDone.'% done');
         });
 
-        $this->line('northstar:default-sms-topics - Added "general" and "voting" to each appropriate user.');
+        $this->line('northstar:default-sms-topics - Added "general" and "voting" to each appropriate user with smsStatus "'.$smsStatus.'".');
     }
 }
