445026ffd031bb2e377cc8736a675a870f7c5b20
David Furnes <david@dfurnes.com>
2020-04-24 15:50:37 -0400

    Add an index for SMS fields.

---
+++ database/migrations/2020_04_24_194151_add_index_to_sms_status_and_topics_fields.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToSmsStatusAndTopicsFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $collection) {
+            $collection->index(['sms_status', 'sms_subscription_topics'], null, null, ['sparse' => true]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $collection) {
+            $collection->dropIndex('sms_status_1_sms_subscription_topics_1');
+        });
+    }
+}
