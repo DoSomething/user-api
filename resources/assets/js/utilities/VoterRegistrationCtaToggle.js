4a07007dd91f33593b723f9516b08c162c9b500b
Lindsay Maher <lindsaymah3r@gmail.com>
2020-07-06 13:05:45 -0500

    adds third voter reg prompt and updates JS file to hide show accordingly. adds a note on the analytics for uncertain voters in prep for new analytics event (#1030)
    Co-authored-by: Lindsay Maher <lmaher@Lindsays-MBP.home>

--- resources/assets/js/utilities/VoterRegistrationCtaToggle.js
+++ resources/assets/js/utilities/VoterRegistrationCtaToggle.js
@@ -6,19 +6,27 @@ const $ = require('jquery');
  */
 
 function clickHandlerToggleContent(event) {
-    const unregisteredContent = document.getElementById('voter-reg-cta-unregistered');
+    const confirmedContent = document.getElementById('voter-reg-cta-confirmed');
     const uncertainContent = document.getElementById('voter-reg-cta-uncertain');
+    const unregisteredContent = document.getElementById('voter-reg-cta-unregistered');
     const value = event.target.value
 
     if(value === 'unregistered') {
         unregisteredContent.classList.remove('hidden')
         uncertainContent.classList.add('hidden')
+        confirmedContent.classList.add('hidden')
     } else if(value === 'uncertain') {
         uncertainContent.classList.remove('hidden')
         unregisteredContent.classList.add('hidden')
+        confirmedContent.classList.add('hidden')
+    } else if(value === 'confirmed') {
+        confirmedContent.classList.remove('hidden')
+        unregisteredContent.classList.add('hidden')
+        uncertainContent.classList.add('hidden')
     } else {
         unregisteredContent.classList.add('hidden');
         uncertainContent.classList.add('hidden');
+        confirmedContent.classList.add('hidden')
     }
 
 }
