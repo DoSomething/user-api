6b610ca56b941ba1407a57f7d9f32c2342451612
David Furnes <david@dfurnes.com>
2020-09-16 11:48:27 -0400

    Add some passing test assertions for  helper.

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -62,4 +62,15 @@ class HelpersTest extends BrowserKitTestCase
         $this->asNormalUser()->getJson('/status');
         $this->assertEquals(client_id(), 'phpunit');
     }
+
+    /** @test */
+    public function testIsDoSomethingDomain()
+    {
+        $this->assertTrue(is_dosomething_domain('https://dosomething.org'), 'It should recognize our base domain.');
+        $this->assertTrue(is_dosomething_domain('https://identity.dosomething.org'), 'It should recognize one of our subdomains.');
+        $this->assertTrue(is_dosomething_domain('https://www.dosomething.org/campaigns/teens-for-jeans'), 'It should recognize a nested path.');
+
+        $this->assertFalse(is_dosomething_domain('https://www.google.com'), 'It should reject a non-DoSomething hostname.');
+        $this->assertFalse(is_dosomething_domain('https://dosomething.org.evil.com'), 'It should reject a non-DoSomething hostname with valid "prefix".');
+    }
 }
