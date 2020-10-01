6882e6b75f8df94c0d70607b64f5421736b9908f
David Furnes <david@dfurnes.com>
2020-10-01 12:15:04 -0400

    Send event directly to Customer.io.

--- app/Jobs/SendPasswordResetToCustomerIo.php
+++ app/Jobs/SendPasswordResetToCustomerIo.php
@@ -4,6 +4,7 @@ namespace Northstar\Jobs;
 
 use Northstar\Models\User;
 use Illuminate\Bus\Queueable;
+use Northstar\Services\CustomerIo;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -65,24 +66,16 @@ class SendPasswordResetToCustomerIo implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(CustomerIo $customerIo)
     {
-        // Rate limit Blink/Customer.io API requests to 10/s.
+        // Rate limit Customer.io API requests to 10/s.
         $throttler = Redis::throttle('customerio')->allow(10)->every(1);
-        $throttler->then(function () {
-            $payload = [
+        $throttler->then(function () use ($customerIo) {
+            $customerIo->trackEvent($this->user, 'track_call_to_action_email', [
                 'actionUrl' => $this->getUrl(),
                 'type' => $this->type,
                 'userId' => $this->user->id,
-            ];
-
-            $shouldSendToCustomerIo = config('features.blink');
-            if ($shouldSendToCustomerIo) {
-                gateway('blink')->userCallToActionEmail($payload);
-            }
-
-            $verb = $shouldSendToCustomerIo ? 'sent' : 'would have been sent';
-            info('Call To Action Email for '.$payload['userId'].' '.$verb.' to Customer.io');
+            ]);
         }, function () {
             // Could not obtain lock... release to the queue.
             return $this->release(10);
