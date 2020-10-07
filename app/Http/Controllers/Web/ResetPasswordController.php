a00cab1e70bb2cb5198f3178c4be4853d523976a
dinnall <michaeldinnall@gmail.com>
2020-10-07 11:59:38 -0400

    Update ResetPasswordController.php

--- app/Http/Controllers/Web/ResetPasswordController.php
+++ app/Http/Controllers/Web/ResetPasswordController.php
@@ -136,7 +136,8 @@ class ResetPasswordController extends Controller
      */
     public function redirectPath()
     {
-        if (Str::contains(request()->type, 'activate-account')) {
+        if (Str::contains(request()->type, 'activate-account')) 
+        {
         return 'profile/about';
         }
         return config('services.phoenix.url').'/next/login';
