abe0fc513e6cbdf953de2e859f884d647f0aee9c
David Furnes <david@dfurnes.com>
2020-12-07 13:25:12 -0500

    Only output errors partial if errors exist.

--- resources/views/forms/errors.blade.php
+++ resources/views/forms/errors.blade.php
@@ -1,8 +1,10 @@
-<div class="validation-error fade-in-up">
-    <h4>{{ trans('auth.validation.issues') }}</h4>
-    <ul class="list -compacted">
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
+@if ($errors->count())
+    <div class="validation-error fade-in-up">
+        <h4>{{ trans('auth.validation.issues') }}</h4>
+        <ul class="list -compacted">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
