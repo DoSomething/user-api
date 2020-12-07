daaff91a9a6fca374712fce8087bf4c3c08c2f90
David Furnes <david@dfurnes.com>
2020-12-07 13:00:23 -0500

    Support requiring role for both API & web guards.

--- app/Http/Middleware/RequireRole.php
+++ app/Http/Middleware/RequireRole.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use App\Auth\Role;
 use Closure;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Auth;
 use League\OAuth2\Server\Exception\OAuthServerException;
 
 class RequireRole
@@ -20,7 +22,21 @@ class RequireRole
      */
     public function handle($request, Closure $next, ...$roles)
     {
-        Role::gate($roles);
+        // If we're using the 'api' driver, then check if we have an
+        // access token with the correct role before continuing:
+        if (Auth::getDefaultDriver() === 'api') {
+            Role::gate($roles);
+
+            return $next($request);
+        }
+
+        // Otherwise, this is a traditional web session. Check if we're
+        // logged in & have one of the required roles. If not, throw:
+        if (Auth::guest() || !Auth::user()->hasRole(...$roles)) {
+            throw new AuthorizationException(
+                'You don\'t have the correct role to do that!',
+            );
+        }
 
         return $next($request);
     }
