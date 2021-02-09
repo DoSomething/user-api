0342b7744c2d3cf4f607499eb740c6af0b190d2c
Aaron Schachter <aaronschachter@gmail.com>
2021-02-08 21:20:40 -0800

    Deprecates SendPasswordUpdatedToCustomerIo.php

--- app/Listeners/ReportPasswordUpdated.php
+++ app/Listeners/ReportPasswordUpdated.php
@@ -3,7 +3,8 @@
 namespace App\Listeners;
 
 use App\Events\PasswordUpdated;
-use App\Jobs\SendPasswordUpdatedToCustomerIo;
+use App\Jobs\CreateCustomerIoEvent;
+use App\Jobs\SendCustomerIoEmail;
 
 class ReportPasswordUpdated
 {
@@ -15,9 +16,28 @@ class ReportPasswordUpdated
      */
     public function handle(PasswordUpdated $event)
     {
-        SendPasswordUpdatedToCustomerIo::dispatch(
-            $event->user,
-            $event->updatedVia,
+        /**
+         * Use Customer.io events for updates via account activation, so admins can customize the
+         * user's journey based on their source (e.g., Rock The Vote, newsletter subscription).
+         */
+        if (Str::contains($event->updatedVia, 'activate-account') {
+            return CreateCustomerIoEvent::dispatch(
+                $event->user,
+                /**
+                 * TODO: We should rename this as activate-accounted now that we no longer
+                 * will use Customer.io events for standard forgot-password or profile updates.
+                 */
+                'password_updated'
+                [
+                    'updated_via' => $event->updatedVia,
+                ],
+            );
+        }
+
+        // Use Transactional API for other updates (e..g, profile, forgot password email).
+        return SendCustomerIoEmail(
+            $event->user->email,
+            config('services.customerio.app_api.transactional_message_ids.password_updated'),
         );
     }
 }
---
+++ documentation/customer-io.md
@@ -0,0 +1,11 @@
+# Customer.io
+
+We integrate with Customer.io to handle sending emails to members.
+
+## Track API
+
+Used to track member activity in Customer.io, and send emails custom to campaigns.
+
+## App API
+
+Used to send transactional emails for forgot password requests, and password updated events.
