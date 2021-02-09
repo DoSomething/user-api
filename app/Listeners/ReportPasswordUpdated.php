c4fa7385b0e217cdbe4cdab554372192c5f284bb
Aaron Schachter <aaronschachter@gmail.com>
2021-02-09 03:12:10 -0800

    Bug fix

--- app/Listeners/ReportPasswordUpdated.php
+++ app/Listeners/ReportPasswordUpdated.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\PasswordUpdated;
 use App\Jobs\CreateCustomerIoEvent;
 use App\Jobs\SendCustomerIoEmail;
+use Illuminate\Support\Str;
 
 class ReportPasswordUpdated
 {
@@ -16,28 +17,28 @@ class ReportPasswordUpdated
      */
     public function handle(PasswordUpdated $event)
     {
-        /**
+        /*
          * Use Customer.io events for updates via account activation, so admins can customize the
          * user's journey based on their source (e.g., Rock The Vote, newsletter subscription).
          */
-        if (Str::contains($event->updatedVia, 'activate-account') {
+        if (Str::contains($event->updatedVia, 'activate-account')) {
             return CreateCustomerIoEvent::dispatch(
                 $event->user,
-                /**
+                /*
                  * TODO: We should rename this as activate-accounted now that we no longer
                  * will use Customer.io events for standard forgot-password or profile updates.
                  */
-                'password_updated'
+                'password_updated',
                 [
                     'updated_via' => $event->updatedVia,
-                ],
+                ]
             );
         }
 
         // Use Transactional API for other updates (e..g, profile, forgot password email).
-        return SendCustomerIoEmail(
+        SendCustomerIoEmail::dispatch(
             $event->user->email,
-            config('services.customerio.app_api.transactional_message_ids.password_updated'),
+            config('services.customerio.app_api.transactional_message_ids.password_updated')
         );
     }
 }
--- documentation/customer-io.md
+++ documentation/customer-io.md
@@ -1,11 +1,13 @@
 # Customer.io
 
-We integrate with Customer.io to handle sending emails to members.
+We integrate with Customer.io to send transactional and promotional messaging to members.
 
 ## Track API
 
-Used to track member activity in Customer.io, and send emails custom to campaigns.
+We use the [Track API](https://customer.io/docs/api/#tag/trackOverview) to track member activity in Customer.io, and send both promotional and transactional emails to subscribers.
 
 ## App API
 
-Used to send transactional emails for forgot password requests, and password updated events.
+We use the [App API](https://customer.io/docs/api/#tag/appOverview) to send transactional emails for forgot password requests, and password updated events.
+
+We use the [send email](https://customer.io/docs/api/#operation/sendEmail) endpoint to send these emails without maintaining a profile for members who have triggered these events. This is done by executing all send email requests for a specific placeholder Customer.io profile we've set up.
