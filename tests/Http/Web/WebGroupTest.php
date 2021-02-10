549428bdcd56975b0d1ba2e0cb18dcd8b90e7758
Diego Lorenzo <diego@diegolorenzo.com>
2021-02-09 19:14:31 -0500

    Adding Web Group tests.

---
+++ tests/Http/Web/WebGroupTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Models\Group;
+use App\Models\GroupType;
+use App\Models\User;
+
+class WebGroupTest extends TestCase
+{
+    /** @test */
+    public function testAdminCanCreateGroup()
+    {
+        $admin = factory(User::class, 'admin')->create();
+
+        $groupType = factory(GroupType::class)->create();
+
+        $name = $this->faker->sentence;
+
+        $response = $this->actingAs($admin, 'web')->post('/groups', [
+            'group_type_id' => $groupType->id,
+            'name' => $name,
+        ]);
+
+        $response->assertRedirect();
+
+        $this->assertMysqlDatabaseHas('groups', [
+            'group_type_id' => $groupType->id,
+            'name' => $name,
+        ]);
+    }
+
+    /** @test */
+    public function testAdminCannotCreateDuplicateGroup()
+    {
+        $admin = factory(User::class, 'admin')->create();
+
+        $group = factory(Group::class)->create();
+
+        $response = $this->actingAs($admin, 'web')->post('groups', [
+            'group_type_id' => $group->group_type_id,
+            'name' => $group->name,
+        ]);
+
+        // Validation fails due to duplicate resource.
+        $response->assertSessionHasErrors([
+            'name' => 'The name has already been taken.',
+        ]);
+    }
+
+    /** @test */
+    public function testStaffCanCreateGroup()
+    {
+        $staff = factory(User::class, 'staff')->create();
+
+        $groupType = factory(GroupType::class)->create();
+
+        $name = $this->faker->sentence;
+
+        $response = $this->actingAs($staff, 'web')->post('groups', [
+            'group_type_id' => $groupType->id,
+            'name' => $name,
+        ]);
+
+        $response->assertRedirect();
+
+        $this->assertMysqlDatabaseHas('groups', [
+            'group_type_id' => $groupType->id,
+            'name' => $name,
+        ]);
+    }
+}
