f1d014a3e8b88f7a2d479521a2084efc46934974
Aaron Schachter <aaronschachter@gmail.com>
2021-02-24 10:56:37 -0800

    Adds POST /users/:id/promotions as Chompy workaround

--- routes/api.php
+++ routes/api.php
@@ -100,6 +100,12 @@ Route::group(['prefix' => 'v2', 'as' => 'v2.'], function () {
 
     // Promotions
     Route::delete('users/{user}/promotions', 'PromotionsController@destroy');
+    /*
+     * HACK: Gateway PHP does not correctly parse our delete request response, so expose a POST
+     * route to mute promotions and inspect the response.
+     * @see https://github.com/DoSomething/chompy/pull/202
+     */
+    Route::post('users/{user}/promotions', 'PromotionsController@destroy');
 
     // Cause Preferences
     Route::post('users/{user}/causes/{cause}', 'CauseUpdateController@store');
