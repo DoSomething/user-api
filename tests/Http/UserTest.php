39306ab519aa563a81d89eefde2f9655b44a07e3
Lindsay Maher <lindsaymah3r@gmail.com>
2021-03-08 12:28:02 -0500

    remove exception handling from test

--- tests/Http/UserTest.php
+++ tests/Http/UserTest.php
@@ -549,8 +549,6 @@ class UserTest extends TestCase
      */
     public function testV2CreateUser()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->asAdminUser()->json('POST', 'v2/users', [
             'first_name' => 'Hercules',
             'last_name' => 'Mulligan',
