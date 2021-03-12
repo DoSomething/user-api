8935ee56d26b471081972cea38908eb25d7fcea2
David Furnes <david@dfurnes.com>
2021-03-12 14:55:25 -0500

    Fix volunteer credit PDF & scholarship newsletter signup. (#1153)
    * Re-install CORS package.
    
    * Use wildcard access control on images.
    
    * Allow 'phoenix.test' as an origin...?
    
    * Add CORS header to our 'v2/' and 'v1/' endpoints.
    
    * Hmm, limit CORS to users & subscriptions resources for now...

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -3,15 +3,38 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Fruitcake\Cors\HandleCors as BaseMiddleware;
 
-class HandleCors
+class HandleCors extends BaseMiddleware
 {
+    /**
+     * Set a wildcard access control header for the following paths.
+     *
+     * @var string[]
+     */
+    protected $stars = ['images/*'];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure $next
+     */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        // If this request matches any paths in the "stars" array, attach a
+        // wildcard allowed origin. This allows pages from *any* domain to
+        // embed/access this resource:
+        if ($request->is(...$this->stars)) {
+            $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Origin', '*');
 
-        return $response;
+            return $response;
+        }
+
+        // Otherwise, we'll default to attaching origin-specific access control
+        // headers (e.g. 'Access-Control-Allow-Origin: www.dosomething.org'):
+        return parent::handle($request, $next);
     }
 }
