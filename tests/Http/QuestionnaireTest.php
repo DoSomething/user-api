36e49bbbe8413d2fb8ed157d2ffc2ca607d27f87
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-03-17 13:30:30 -0400

    Add tests for /questionnaires endpoint

---
+++ tests/Http/QuestionnaireTest.php
@@ -0,0 +1,200 @@
+<?php
+
+use App\Models\Action;
+use App\Models\Campaign;
+use App\Models\Club;
+use App\Models\Group;
+use App\Models\Post;
+use App\Models\Reaction;
+use App\Models\Signup;
+use App\Models\User;
+use App\Services\CustomerIo;
+use App\Services\Fastly;
+use App\Services\GraphQL;
+use Illuminate\Http\UploadedFile;
+
+class QuestionnaireTest extends TestCase
+{
+    /**
+     * Helper function to assert post structure.
+     */
+    public function assertPostStructure($response)
+    {
+        return $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'signup_id',
+                'northstar_id',
+                'type',
+                'action',
+                'media' => ['url', 'original_image_url', 'text'],
+                'quantity',
+                'hours_spent',
+                'tags' => [],
+                'reactions' => ['reacted', 'total'],
+                'status',
+                'details',
+                'location',
+                'school_id',
+                'club_id',
+                'source',
+                'remote_addr',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    /**
+     * Test that a POST request to /posts creates a new
+     * post and signup, if it doesn't already exist.
+     *
+     * @return void
+     */
+    public function testCreatingQuestionnairePostsAndSignups()
+    {
+        $user = factory(User::class)->create();
+        $referrerUser = factory(User::class)->create();
+
+        $campaignId = factory(Campaign::class)->create()->id;
+        $location = 'US-' . $this->faker->stateAbbr();
+        $school_id = $this->faker->word;
+        $details = ['source-detail' => 'broadcast-123', 'other' => 'other'];
+        $groupId = factory(Group::class)->create()->id;
+
+        $contentfulId = '123';
+
+        $actionOne = factory(Action::class)->create([
+            'campaign_id' => $campaignId,
+            'post_type' => 'text',
+        ]);
+
+        $questionOne = ['title' => 'Question one.', 'answer' => 'Answer one.', 'action_id' =>  $actionOne->id];
+
+        $actionTwo = factory(Action::class)->create([
+            'campaign_id' => $campaignId,
+            'post_type' => 'text',
+        ]);
+
+        $questionTwo = ['title' => 'Question two.', 'answer' => 'Answer two.', 'action_id' =>  $actionTwo->id];
+
+        $response = $this->asUser($user)->json('POST', 'api/v3/questionnaires', [
+            'questions' => [$questionOne, $questionTwo],
+            'contentful_id' => $contentfulId,
+            'location' => $location,
+            'school_id' => $school_id,
+            'details' => json_encode($details),
+            'referrer_user_id' => $referrerUser->id,
+            'group_id' => $groupId,
+        ]);
+
+        $response->assertStatus(201)->assertJson([
+            'data' => [
+                [
+                    'action_id' => $questionOne['action_id'],
+                    'media' => ['text' => $questionOne['answer']],
+                ],
+                [
+                    'action_id' => $questionTwo['action_id'],
+                    'media' => ['text' => $questionTwo['answer']],
+                ],
+            ],
+        ]);
+
+        // Make sure the signup is persisted to the database.
+        $this->assertMysqlDatabaseHas('signups', [
+            'campaign_id' => $campaignId,
+            'northstar_id' => $user->id,
+            'referrer_user_id' => $referrerUser->id,
+            'group_id' => $groupId,
+        ]);
+
+        // Make sure two posts are persisted to the database.
+        $this->assertMysqlDatabaseHas('posts',
+        [
+            'northstar_id' => $user->id,
+            'campaign_id' => $campaignId,
+            'type' => 'text',
+            'action' => $actionOne->name,
+            'action_id' => $actionOne->id,
+            'text' => $questionOne['answer'],
+            'status' => 'pending',
+            'location' => $location,
+            'school_id' => $school_id,
+            'details' => json_encode(array_merge($details, [
+                'questionnaire' => true,
+                'question' => $questionOne['title'],
+                'contentful_id' => $contentfulId,
+            ])),
+            'referrer_user_id' => $referrerUser->id,
+            'group_id' => $groupId,
+        ]);
+
+        $this->assertMysqlDatabaseHas('posts', [
+            'northstar_id' => $user->id,
+            'campaign_id' => $campaignId,
+            'type' => 'text',
+            'action' => $actionTwo->name,
+            'action_id' => $actionTwo->id,
+            'text' => $questionTwo['answer'],
+            'status' => 'pending',
+            'location' => $location,
+            'school_id' => $school_id,
+            'details' => json_encode(array_merge($details, [
+                'questionnaire' => true,
+                'question' => $questionTwo['title'],
+                'contentful_id' => $contentfulId,
+            ])),
+            'referrer_user_id' => $referrerUser->id,
+            'group_id' => $groupId,
+        ]);
+    }
+
+    /**
+     * Test validation for creating a post.
+     *
+     * POST /api/v3/posts
+     * @return void
+     */
+    public function testCreatingAQuestionnaireWithValidationErrors()
+    {
+        $user = factory(User::class)->create();
+        $action = factory(Action::class)->create([
+            'post_type' => 'text',
+        ]);
+
+        $response = $this->asUser($user)->postJson('api/v3/questionnaires', [
+            'action_id' => $action->id,
+            'type' => 'text',
+            'questions' => [
+                [
+                    // Should be a valid Action ID:
+                    'action_id' => null,
+                    // Should both be Strings:
+                    'title' => 1,
+                    'answer' => 2,
+                ],
+            ],
+            // and we've omitted the required 'contentful_id' field!
+        ]);
+
+        $response->assertJsonValidationErrors([
+            'contentful_id',
+            'questions.0.action_id',
+            'questions.0.title',
+            'questions.0.answer',
+        ]);
+    }
+
+    /**
+     * Test that non-authenticated user's/apps can't create a questionnaire.
+     *
+     * @return void
+     */
+    public function testUnauthenticatedUserCreatingAQuestionnaire()
+    {
+        $response = $this->postJson('api/v3/posts', []);
+
+        $response->assertUnauthorized();
+    }
+}
