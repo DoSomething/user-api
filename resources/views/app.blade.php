e99a8759845790c11408735468ad2813c8fda6f4
Diego Lorenzo <diego@diegolorenzo.com>
2021-03-18 20:13:01 -0400

    Configuring Inertia.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>
+            {{ isset($title) ? $title . ' | Administration' : 'Administration' }}
+        </title>
+
+        <link rel="apple-touch-icon-precomposed" href="/assets/images/apple-touch-icon-precomposed.png">
+
+        {{-- <link rel="stylesheet" href="{{ elixir('inertia.css', 'dist') }}"> --}}
+
+        <script src="{{ elixir('inertia.js', 'dist') }}" defer></script>
+    </head>
+
+    <body>
+        @inertia
+    </body>
+</html>
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,12 @@ module.exports = configure({
   entry: {
     app: './resources/assets/js/app.js',
     admin: './resources/assets/admin/bootstrap.js',
+    inertia: './resources/js/app.js',
   },
   output: {
     filename: '[name]-[chunkhash].js',
     // Override output path for Laravel's "public" directory.
     path: path.join(__dirname, '/public/dist'),
+    publicPath: '/dist/',
   },
 });
