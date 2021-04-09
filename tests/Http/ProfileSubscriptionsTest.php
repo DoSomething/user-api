84a3cfca08209ec77e68d5a8c4dd382d7ceaf682
katiecrane <kcrane@dosomething.org>
2021-04-09 15:59:07 -0600

    Test for unsetting topics in registration flow

--- tests/Http/ProfileSubscriptionsTest.php
+++ tests/Http/ProfileSubscriptionsTest.php
@@ -62,6 +62,31 @@ class ProfileSubscriptionsTest extends BrowserKitTestCase
     }
 
     /**
+     * Test that users can successfully set no newsletters.
+     */
+    public function testSelectingNoNewsletters()
+    {
+        // Create a user with the "community" topic just like in registerViaWeb
+        $user = factory(User::class)->create([
+            'sms_status' => null,
+            'mobile' => null,
+            'email_subscription_topics' => ['community']
+        ]);
+
+        // Go to the third page of the registration flow
+        $this->be($user, 'web');
+
+        $this->visit('/profile/subscriptions')
+            ->uncheck('email_subscription_topics[0]')
+            ->press('Finish');
+
+        // Make sure the user has no email topics set
+        $updatedUser = $user->fresh();
+
+        $this->assertEquals([], $updatedUser->email_subscription_topics);
+    }
+
+    /**
      * Test that users can move to the next step of registration without updating any fields.
      */
     public function testFinishButtonWithoutUpdates()
