bbbd42a5a89cc7ff56b9bf3079aaedf2e5429fe3
Diego Lorenzo <diego@diegolorenzo.com>
2021-04-16 15:33:07 -0500

    Adding Types from Chompy.

---
+++ app/Types/ImportType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+class ImportType
+{
+    /**
+     * An email subscription import.
+     *
+     * @var string
+     */
+    public static $emailSubscription = 'email-subscription';
+
+    /**
+     * A mute promotions import.
+     *
+     * @var string
+     */
+    public static $mutePromotions = 'mute-promotions';
+
+    /**
+     * A Rock The Vote import.
+     *
+     * @var string
+     */
+    public static $rockTheVote = 'rock-the-vote';
+
+    /**
+     * Returns config array of given import type.
+     *
+     * @return array
+     */
+    public static function getConfig($type)
+    {
+        if ($type === self::$emailSubscription) {
+            return config('import.email_subscription');
+        }
+
+        if ($type === self::$rockTheVote) {
+            return config('import.rock_the_vote');
+        }
+
+        return [];
+    }
+}
