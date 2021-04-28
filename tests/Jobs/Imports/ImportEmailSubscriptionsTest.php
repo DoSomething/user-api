371fec313a00f2d183053e23118fa7639870c88d
Diego Lorenzo <diego@diegolorenzo.com>
2021-04-28 16:57:50 -0500

    Adding tests for the ImportEmailSubscriptions job.

--- tests/Jobs/Imports/ImportEmailSubscriptionsTest.php
+++ tests/Jobs/Imports/ImportEmailSubscriptionsTest.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Auth\Registrar;
+use App\Jobs\CreateCustomerIoEvent;
 use App\Jobs\Imports\ImportEmailSubscriptions;
+use App\Jobs\SendForgotPasswordEmail;
 use App\Models\ImportFile;
 use App\Models\User;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Bus;
 
 class ImportEmailSubscriptionsTest extends TestCase
 {
@@ -10,7 +15,7 @@ class ImportEmailSubscriptionsTest extends TestCase
      * Test that an existing user record can have email subcriptions updated
      * when importing records.
      */
-    public function testImportingEmailSubscriptionForExistingUser()
+    public function testAddsSubscriptionForExistingUser()
     {
         $user = factory(User::class)->create();
 
@@ -42,7 +47,7 @@ class ImportEmailSubscriptionsTest extends TestCase
      * Test that importing a record for a new user creates a user record and
      * adds their email subscription.
      */
-    public function testImportingEmailSubcriptionForNewUser()
+    public function testAddsSubcriptionForNewUser()
     {
         $importFile = factory(ImportFile::class)
             ->states('email_subscription')
@@ -70,5 +75,67 @@ class ImportEmailSubscriptionsTest extends TestCase
             'email_subscription_topics' => ['lifestyle'],
             'first_name' => 'puppet',
         ]);
+    }
+
+    /**
+     * Test that importing an email subscription for existing user with prior subscription
+     * topics, appends the new one to the list.
+     */
+    public function testNoDuplicateEmailSubscriptionTopics()
+    {
+        $user = factory(User::class)
+            ->states('email-subscribed-community')
+            ->create();
+
+        $importFile = factory(ImportFile::class)
+            ->states('email_subscription')
+            ->create();
+
+        ImportEmailSubscriptions::dispatch(
+            ['email' => $user->email, 'first_name' => $user->first_name],
+            $importFile,
+            [
+                'source_detail' => 'phpunit',
+                'email_subscription_topic' => 'community',
+            ],
+        );
+
+        $this->assertMysqlDatabaseHas('import_files', [
+            'import_count' => 1,
+            'import_type' => 'email-subscription',
+        ]);
+
+        $this->assertMongoDatabaseHas('users', [
+            '_id' => $user->id,
+            'email_subscription_topics' => ['community'],
+        ]);
+    }
+
+    /**
+     * Test that importing an email subscription for a new user triggers password reset.
+     */
+    public function testImportTriggersPasswordResetForNewUser()
+    {
+        $importFile = factory(ImportFile::class)
+            ->states('email_subscription')
+            ->create();
+
+        ImportEmailSubscriptions::dispatch(
+            [
+                'email' => 'puppetsloth@dosomething.org',
+                'first_name' => 'puppet',
+            ],
+            $importFile,
+            [
+                'source_detail' => 'phpunit',
+                'email_subscription_topic' => 'community',
+            ],
+        );
+
+        $user = App::make(Registrar::class)->resolve([
+            'email' => 'puppetsloth@dosomething.org',
+        ]);
+
+        $this->assertCustomerIoEvent($user, 'call_to_action_email');
     }
 }
