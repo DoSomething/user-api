8b8a788409bb55d10a4e164d6fbd9ada33fba207
Lindsay Maher <lindsaymah3r@gmail.com>
2021-05-04 14:57:29 -0400

    creates migration file for new impact goal field

---
+++ database/migrations/2021_05_04_185150_add_impact_goal_to_actions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImpactGoalToActions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table
+                ->string('impact_goal')
+                ->after('quiz')
+                ->nullable()
+                ->comment('The impact goal associated with this action.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->dropColumn('impact_goal');
+        });
+    }
+}
