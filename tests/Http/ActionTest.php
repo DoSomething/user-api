d1e8462e0e3ab47e96ab39a090660a95a91f0dd3
Lindsay Maher <lindsaymah3r@gmail.com>
2021-05-06 13:33:21 -0400

    validation added for impact goal and formatting issues

--- tests/Http/ActionTest.php
+++ tests/Http/ActionTest.php
@@ -40,23 +40,17 @@ class ActionTest extends TestCase
     }
 
     /**
-     * Test that when an action is created with an impact goal it saves properly
+     * Test that when an action is created with an impact goal it saves properly.
      *
      * @return void
      */
-    public function testCreatingAnActionWithAnImpactGoal()
+    public function testDisplayingActionWithAnImpactGoal()
     {
         $action = factory(Action::class)->create(['impact_goal' => 3000]);
 
         $response = $this->getJson('api/v3/actions/' . $action->id);
 
         $response->assertOk();
-
-        $this->assertMysqlDatabaseHas('actions', [
-            'post_type' => $action->post_type,
-            'name' => $action->name,
-            'id' => $action->id,
-            'impact_goal' => $action->impact_goal,
-        ]);
+        $response->assertJsonPath('data.impact_goal', $action->impact_goal);
     }
 }
