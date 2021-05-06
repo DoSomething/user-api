b35cf942670781383a1a7ae885aaf558a251d0fa
David Furnes <david@dfurnes.com>
2021-05-06 17:56:58 -0400

    Fix SMS statuses by ID, rather than by querying database.

--- app/Console/Commands/FixSmsStatusCommand.php
+++ app/Console/Commands/FixSmsStatusCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Models\User;
 use App\Services\CustomerIo;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
+use League\Csv\Reader;
 
 class FixSmsStatusCommand extends Command
 {
@@ -14,14 +14,14 @@ class FixSmsStatusCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'northstar:fix-sms-status {--dry-run}';
+    protected $signature = 'northstar:fix-sms-status {--dry-run} {input=php://stdin}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Fix accounts with active sms_status but no mobile number.';
+    protected $description = 'Unset sms_status for the given accounts.';
 
     /**
      * Execute the console command.
@@ -32,36 +32,47 @@ class FixSmsStatusCommand extends Command
     {
         info('northstar:fix-sms-status - Starting up!');
 
-        // Get addressable users with a null mobile:
-        $query = User::whereIn('sms_status', [
-            'active',
-            'pending',
-            'less',
-        ])->where('mobile', 'exists', false);
+        $input = file_get_contents($this->argument('input'));
+        $csv = Reader::createFromString($input);
+        $csv->setHeaderOffset(0);
 
-        $query->chunkById(200, function (Collection $users) use ($customerIo) {
-            foreach ($users as $user) {
-                $profile = $customerIo->getAttributes($user);
+        foreach ($csv->getRecords() as $record) {
+            $user = User::find($record['id']);
 
-                throttle(600); // Customer.io Beta API is rate-limited, so keep under 10/s (600/min).
+            if (!$user) {
+                info('Could not find user', ['id' => $record['id']]);
 
-                // We want to log a little context on each user that we're fixing
-                // to help identify patterns of potentially affected accounts:
-                info('Fixing user', [
-                    'id' => $user->id,
-                    'has_profile' => !empty($profile),
-                    'phone' => data_get($profile, 'phone'),
-                    'source' => $user->source,
-                    'created_at' => $user->created_at,
-                ]);
-
-                if ($this->option('dry-run')) {
-                    continue;
-                }
-
-                $user->unset('sms_status');
+                continue;
             }
-        });
+
+            // If this user has a mobile (perhaps due to adding one after
+            // our CSV was generated), skip unsetting their SMS status:
+            if (!is_null($user->mobile)) {
+                info('Skipping user', ['id' => $user->id]);
+
+                continue;
+            }
+
+            $profile = $customerIo->getAttributes($user);
+
+            throttle(600); // Customer.io Beta API is rate-limited, so keep under 10/s (600/min).
+
+            // We want to log a little context on each user that we're fixing
+            // to help identify patterns of potentially affected accounts:
+            info('Fixing user', [
+                'id' => $user->id,
+                'has_profile' => !empty($profile),
+                'phone' => data_get($profile, 'phone'),
+                'source' => $user->source,
+                'created_at' => $user->created_at,
+            ]);
+
+            if ($this->option('dry-run')) {
+                continue;
+            }
+
+            $user->unset('sms_status');
+        }
 
         info('northstar:fix-sms-status - All done!');
     }
