7d4b7f7cf57fb6e28f2a17c2b2fe6f1200e51208
David Furnes <david@dfurnes.com>
2021-05-27 12:40:37 -0400

    Improve readability of API client.

--- app/Services/RockTheVote.php
+++ app/Services/RockTheVote.php
@@ -19,10 +19,10 @@ class RockTheVote
         $config = config('services.rock_the_vote');
 
         $this->client = new \GuzzleHttp\Client([
-            'base_uri' => $config['url'] . '/api/v4/',
+            'base_uri' => $config['url'],
         ]);
 
-        $this->authQuery = [
+        $this->authParams = [
             'partner_API_key' => $config['api_key'],
             'partner_id' => $config['partner_id'],
         ];
@@ -37,8 +37,8 @@ class RockTheVote
      */
     public function createReport($params)
     {
-        $response = $this->client->post('registrant_reports', [
-            'json' => array_merge($params, $this->authQuery),
+        $response = $this->client->post('/api/v4/registrant_reports', [
+            'json' => array_merge($params, $this->authParams),
         ]);
 
         return json_decode($response->getBody()->getContents(), true);
@@ -53,8 +53,8 @@ class RockTheVote
      */
     public function getReportStatusById($id)
     {
-        $response = $this->client->get('registrant_reports/' . $id, [
-            'query' => $this->authQuery,
+        $response = $this->client->get('/api/v4/registrant_reports/' . $id, [
+            'query' => $this->authParams,
         ]);
 
         return json_decode($response->getBody()->getContents(), true);
@@ -69,7 +69,7 @@ class RockTheVote
     public function getReportByUrl($url)
     {
         $response = $this->client->get($url, [
-            'query' => $this->authQuery,
+            'query' => $this->authParams,
         ]);
 
         return $response->getBody();
