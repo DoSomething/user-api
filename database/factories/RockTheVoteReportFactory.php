f1906726a92cbdd931371d471d356510319c4d47
David Furnes <david@dfurnes.com>
2021-06-03 14:36:54 -0400

    Add some more defaults to RockTheVoteReport factory.

--- database/factories/RockTheVoteReportFactory.php
+++ database/factories/RockTheVoteReportFactory.php
@@ -9,5 +9,8 @@ $factory->define(RockTheVoteReport::class, function (Faker $faker) {
     return [
         'id' => $faker->randomDigitNotNull,
         'status' => 'queued',
+        'current_index' => 0,
+        'since' => '2021-05-26 10:07:00',
+        'before' => '2021-05-26 11:37:00',
     ];
 });
