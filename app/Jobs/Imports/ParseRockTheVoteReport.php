e134931e0c88053c0d132d359044c5d5a03fd256
David Furnes <david@dfurnes.com>
2021-06-03 14:40:39 -0400

    Add "placeholder" ParseRockTheVoteReport job.

---
+++ app/Jobs/Imports/ParseRockTheVoteReport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs\Imports;
+
+use App\Jobs\Job;
+
+class ParseRockTheVoteReport extends Job
+{
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // ...
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        // ...
+    }
+
+    /**
+     * Returns the parameters passed to this job.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return [];
+    }
+}
