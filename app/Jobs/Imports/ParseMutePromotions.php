1297dbbba66be3861b317abd68b86ca6fb67aa74
David Furnes <david@dfurnes.com>
2021-06-09 11:15:03 -0400

    Add 'ParseMutePromotions' job & test.

---
+++ app/Jobs/Imports/ParseMutePromotions.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Jobs\Imports;
+
+use App\Jobs\Job;
+use App\Models\ImportFile;
+use App\Models\User;
+use App\Types\ImportType;
+use Illuminate\Support\Facades\Storage;
+
+class ParseMutePromotions extends Job
+{
+    /**
+     * The path to the stored csv.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Optionally, the user that triggered this.
+     *
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string $path
+     * @param User $user
+     * @return void
+     */
+    public function __construct(string $path, ?User $user = null)
+    {
+        $this->path = $path;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        info('Parsing mute promotions CSV', ['path' => $this->path]);
+
+        $records = read_csv($this->path);
+
+        $importFile = ImportFile::create([
+            'filepath' => $this->path,
+            'import_type' => ImportType::$mutePromotions,
+            'row_count' => iterator_count($records),
+            'user_id' => optional($this->user)->id,
+        ]);
+
+        foreach ($records as $record) {
+            ImportMutePromotions::dispatch($record, $importFile);
+        }
+
+        // Now that we've chomped, delete the import file.
+        Storage::delete($this->path);
+    }
+
+    /**
+     * Returns the parameters passed to this job.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return [];
+    }
+}
---
+++ tests/Jobs/Imports/ParseMutePromotionsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Jobs\Imports\ImportMutePromotions;
+use App\Jobs\Imports\ParseMutePromotions;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Storage;
+
+class ParseMutePromotionsTest extends TestCase
+{
+    /**
+     * Test that this job can parse an uploaded CSV.
+     *
+     * @return void
+     */
+    public function testParsesFile()
+    {
+        Bus::fake();
+        Storage::fake();
+
+        // Create a faked record that we can safely read & delete:
+        $csv = file_get_contents('tests/Jobs/Imports/example-mute.csv');
+        Storage::put('mute-promotions.csv', $csv);
+
+        $this->forceDispatch(new ParseMutePromotions('mute-promotions.csv'));
+
+        // There are 6 rows in the example CSV, so we should dispatch 6 jobs!
+        Bus::assertDispatchedTimes(ImportMutePromotions::class, 6);
+
+        // Then, we should clean up after ourselves.
+        Storage::assertMissing('mute-promotions.csv');
+
+        $this->assertMysqlDatabaseHas('import_files', [
+            'import_type' => 'mute-promotions',
+            'filepath' => 'mute-promotions.csv',
+            'row_count' => 6,
+        ]);
+    }
+}
