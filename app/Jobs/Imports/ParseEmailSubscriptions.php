c3106b44b931b46acf72cd57719d346680cdd2e5
David Furnes <david@dfurnes.com>
2021-06-09 11:14:37 -0400

    Add 'ParseEmailSubscriptions' job & test.

---
+++ app/Jobs/Imports/ParseEmailSubscriptions.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Jobs\Imports;
+
+use App\Jobs\Job;
+use App\Models\ImportFile;
+use App\Models\User;
+use App\Types\ImportType;
+use Illuminate\Support\Facades\Storage;
+
+class ParseEmailSubscriptions extends Job
+{
+    /**
+     * The path to the stored csv.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * The options for this subscription import, for example
+     * 'email_subscription_topic' or 'source_detail'.
+     *
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * Optionally, the user that triggered this.
+     *
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string $path
+     * @param User $user
+     * @return void
+     */
+    public function __construct(
+        string $path,
+        ?array $options = [],
+        ?User $user = null
+    ) {
+        $this->path = $path;
+        $this->options = $options;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        info('Parsing email subscriptions CSV', [
+            'path' => $this->path,
+            'options' => $this->options,
+        ]);
+
+        $records = read_csv($this->path);
+
+        $importFile = ImportFile::create([
+            'filepath' => $this->path,
+            'import_type' => ImportType::$emailSubscription,
+            'row_count' => iterator_count($records),
+            'user_id' => optional($this->user)->id,
+        ]);
+
+        foreach ($records as $record) {
+            ImportEmailSubscriptions::dispatch(
+                $record,
+                $importFile,
+                $this->options,
+            );
+        }
+
+        // Now that we've chomped, delete the import file.
+        Storage::delete($this->path);
+    }
+
+    /**
+     * Returns the parameters passed to this job.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return [];
+    }
+}
---
+++ tests/Jobs/Imports/ParseEmailSubscriptionsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Jobs\Imports\ImportEmailSubscriptions;
+use App\Jobs\Imports\ParseEmailSubscriptions;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Storage;
+
+class ParseEmailSubscriptionsTest extends TestCase
+{
+    /**
+     * Test that this job can parse an uploaded CSV.
+     *
+     * @return void
+     */
+    public function testParsesFile()
+    {
+        Bus::fake();
+        Storage::fake();
+
+        // Create a faked record that we can safely read & delete:
+        $csv = file_get_contents('tests/Jobs/Imports/example-email-subs.csv');
+        Storage::put('subscriptions.csv', $csv);
+
+        $this->forceDispatch(
+            new ParseEmailSubscriptions('subscriptions.csv', [
+                'email_subscription_topic' => 'news',
+                'source_detail' => 'phpunit',
+            ]),
+        );
+
+        // There are 3 rows in the example CSV, so we should dispatch 3 jobs!
+        Bus::assertDispatchedTimes(ImportEmailSubscriptions::class, 3);
+
+        // Then, we should clean up after ourselves.
+        Storage::assertMissing('subscriptions.csv');
+
+        $this->assertMysqlDatabaseHas('import_files', [
+            'import_type' => 'email-subscription',
+            'filepath' => 'subscriptions.csv',
+            'row_count' => 3,
+        ]);
+    }
+}
